a24989942a5bfb9ca313018771e0f820b95f255f
cvs <cvs@282e977c-c81d-0410-88c4-b93c2d0d6712>
2000-04-13 07:08:37 +0000

    commands.c, client.h: Change animation code to dynamically allocate space needed for animations instead of using a statically defined/sized array to contain the face information.  This fixes a bug with icors causing the client to crash (it had more animations than the static array had), and this method is just generally a good idea.  Mark Wedel, 4/12/2000

--- trunk/client/commands.c
+++ trunk/client/commands.c
@@ -1,3 +1,30 @@
+/*
+ * static char *rcsid_commands_c =
+ *   "$Id$";
+ */
+/*
+    Crossfire client, a client program for the crossfire program.
+
+    Copyright (C) 2000 Mark Wedel
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+    The author can be reached via e-mail to mwedel@scruz.net
+*/
+
+
 /* Handles commands received by the server.  This does not necessarily
  * handle all commands - some might be in other files (like init.c)
  *
@@ -85,12 +112,12 @@ void AnimCmd(unsigned char *data, int len)
     }
     animations[anum].flags=GetShort_String(data+2);
     animations[anum].num_animations = (len-4)/2;
-    if (animations[anum].num_animations<1 || 
-	animations[anum].num_animations>MAXANIMFACES) {
+    if (animations[anum].num_animations<1) {
 	    fprintf(stderr,"AnimCmd: num animations invalid: %d\n",
 		    animations[anum].num_animations);
 	    return;
     }
+    animations[anum].faces = malloc(sizeof(uint16)*animations[anum].num_animations);
     for (i=4,j=0; i<len; i+=2,j++)
 	animations[anum].faces[j]=GetShort_String(data+i);
 
