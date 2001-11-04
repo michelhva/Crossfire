33125593c29f0a25f4caa99feb01533b8c239f24
mwedel <mwedel@282e977c-c81d-0410-88c4-b93c2d0d6712>
2001-11-04 03:25:59 +0000

    Add copyright comment.  MSW 2001-11-03

--- trunk/client/sound-src/cfsndserv.c
+++ trunk/client/sound-src/cfsndserv.c
@@ -1,4 +1,34 @@
 /*
+ * static char *rcsid_cfsndserv_c =
+ *   "$Id$";
+ */
+/*
+    Crossfire client, a client program for the crossfire program.
+
+    Copyright (C) 2001 Mark Wedel & Crossfire Development Team
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+    The author can be reached via e-mail to crossfire-devel@real-time.com
+*/
+
+/* Comment from the original author is below.  In addition to OSS
+ * and ALSA, sun sound is also supported.
+ */
+
+/*
  * (c) 1998 Jacek Konieczny <jajcus@zeus.polsl.gliwice.pl>
  *
  * This file contains the server for sound support for the client. 
