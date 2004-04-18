ebc58a901a26780dc3fe63da03a107ee6ae6418d
mwedel <mwedel@282e977c-c81d-0410-88c4-b93c2d0d6712>
2004-04-18 06:50:45 +0000

    commit modified ring of occidental mages, which reduces the stats. This should balance the item - much harder to get it to have 30 stats now. Updated version by Lalo Martins, applied by Mark Wedel 2004-04-17

--- trunk/maps/python/ring_occidental_mages.py
+++ trunk/maps/python/ring_occidental_mages.py
@@ -22,12 +22,12 @@ if (CFPython.IsApplied(me)):
     elif (r >= 0.99):
         CFPython.SetIdentified(me,0)
         CFPython.SetCursed(me, 1)
-        CFPython.SetDexterity(me, CFPython.GetDexterity(me)+1)
+        CFPython.SetDexterity(me, CFPython.GetDexterity(me)-1)
     elif (r >= 0.98):
         CFPython.SetIdentified(me,0)
         CFPython.SetCursed(me, 1)
-        CFPython.SetIntelligence(me, CFPython.GetIntelligence(me)+1)
+        CFPython.SetIntelligence(me, CFPython.GetIntelligence(me)-1)
     elif (r >= 0.97):
         CFPython.SetIdentified(me,0)
         CFPython.SetCursed(me, 1)
-        CFPython.SetConstitution(me, CFPython.GetConstitution(me)+1)
+        CFPython.SetConstitution(me, CFPython.GetConstitution(me)-1)
