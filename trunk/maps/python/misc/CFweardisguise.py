45a501986931fc9fd0e3876a543be7920b32d745
temitchell <temitchell@282e977c-c81d-0410-88c4-b93c2d0d6712>
2004-08-02 23:38:50 +0000

    - move event scripts into their own directory - add README - add disguise script (warning usage still subject to bug 878949)

---
+++ trunk/maps/python/misc/CFweardisguise.py
@@ -0,0 +1,31 @@
+#CFweardisguise.py
+# A little script to insert an informational force into the player inventory
+# if a article is applied and remove the force if it is unapplied.
+# For example if you put on a priest robe it will insert the option value into
+# a force slaying field which can be checked against on a map.
+#
+# This script is meant for items that can be worn or carried really
+# I can't say how it will react if you hook it to other types of objects.
+
+import CFPython
+
+import sys
+sys.path.append('%s/%s/python' %(CFPython.GetDataDirectory(),CFPython.GetMapDirectory()))
+
+activator=CFPython.WhoIsActivator()
+activatorname=CFPython.GetName(activator)
+whoami=CFPython.WhoAmI()
+
+option=CFPython.GetEventOptions(whoami,1) # 1 is apply event
+    
+if option:
+    inv = CFPython.CheckInventory(activator, option) #Remove any previous disguise
+    if inv:
+        CFPython.RemoveObject(inv)
+        #print "removing tag"      
+
+    if not CFPython.IsApplied(whoami): #is the object is being applied
+        tag = CFPython.CreateInvisibleObjectInside(activator, option)
+        CFPython.SetName(tag, option)
+        #print "adding tag"
+    
