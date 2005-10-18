ddbccd01d0cf057eddef54ffca488fea021553e0
gros <gros@282e977c-c81d-0410-88c4-b93c2d0d6712>
2005-10-18 18:45:23 +0000

    Adapted the casino-related scripts to CFPython 2.0. Gros - 18/10/2005

--- trunk/maps/python/casino/platinumslots.py
+++ trunk/maps/python/casino/platinumslots.py
@@ -2,24 +2,24 @@
 #to make a new kind of slot machine, copy this file, change the settings and point the slotmachine to the new file.
 # Standard type Platinum Slots
 
-import CFPython
+import Crossfire
 import CFGamble
 import CFItemBroker
 
-activator=CFPython.WhoIsActivator()
-activatorname=CFPython.GetName(activator)
-whoami=CFPython.WhoAmI()
+activator=Crossfire.WhoIsActivator()
+activatorname=activator.Name
+whoami=Crossfire.WhoAmI()
 #gets slot name and adds map name for unique jackpot
-slotname= '%s#%s' %(CFPython.GetName(whoami),CFPython.GetMapPath(CFPython.GetMap(whoami))) 
-x=CFPython.GetXPosition(activator)
-y=CFPython.GetYPosition(activator)
-	
-cointype = "platinacoin" #What type of coin is this slotmachine using?	
-minpot = 200 #Minimum slot jackpot size	
+slotname= '%s#%s' %(whoami.Name,whoami.Map.Path)
+x=activator.X
+y=activator.Y
+
+cointype = "platinacoin" #What type of coin is this slotmachine using?
+minpot = 200 #Minimum slot jackpot size
 maxpot = 100000 #Maxiumum slot jackpot size
 cost = 1 #Price of usage
 
-#Change the items on the slot spinner or the number of items.    
+#Change the items on the slot spinner or the number of items.
 slotlist = ["Jester", "Lord", "Lady", "Prince", "Princess", "King", "Queen", "JackPot"]
 
 spinners = 4 #How many spinners on the slotmachine?
@@ -27,12 +27,12 @@ spinners = 4 #How many spinners on the slotmachine?
 
 Slots=CFGamble.SlotMachine(slotname,slotlist,minpot,maxpot)
 
-if (CFPython.PayAmount(activator, cost*50)):#platinumcoin
+if (activator.PayAmount(cost*50)):#platinumcoin
    Slots.placebet(cost)
    results = Slots.spin(spinners)
    pay = 0
    pot = Slots.checkslot()
-   CFPython.Write('%s' %results, activator, 7)
+   activator.Write('%s' %results, 7)
    for item in results:
       #match all but one - pays out by coin e.g 3 to 1 or 4 to 1
       if results.count(item) == spinners-1:
@@ -54,19 +54,19 @@ if (CFPython.PayAmount(activator, cost*50)):#platinumcoin
             pay = 25
          else:
 	    break
-	 CFPython.Write("%d %ss, a minor win!" %(spinners-1,item),activator)
+	 activator.Write("%d %ss, a minor win!" %(spinners-1,item))
          payoff = cost*pay
          Slots.payoff(payoff)
-         id = CFPython.CreateObject(cointype, (x, y))
+         id = activator.Map.CreateObject(cointype, x, y)
          CFItemBroker.Item(id).add(payoff)
          if payoff == 1:
             message = "you win %d %s!" %(payoff,cointype)
 	 else:
-	    message = "You win %d %ss!!" %(payoff,cointype)	
+	    message = "You win %d %ss!!" %(payoff,cointype)
          break
       elif results.count(item) == spinners:
          #all match - pays out as percent of pot
-         CFPython.Write('%d %ss, a Major win!' %(spinners,item),activator)
+         activator.Write('%d %ss, a Major win!' %(spinners,item))
          if item == "Jester":
             pay = .1
          elif item == "Lord":
@@ -85,16 +85,16 @@ if (CFPython.PayAmount(activator, cost*50)):#platinumcoin
             pay = 1
          payoff = pot*pay
          Slots.payoff(payoff)
-         id = CFPython.CreateObject(cointype, (x, y))
+         id = activator.Map.CreateObject(cointype, x, y)
          CFItemBroker.Item(id).add(payoff)
          if payoff == 1:
             message = "you win %d %s!" %(payoff,cointype)
 	 else:
-	    message = "You win %d %ss!!" %(payoff,cointype)	
+	    message = "You win %d %ss!!" %(payoff,cointype)
          break
       else:
          message = "Better luck next time!"
-   CFPython.Write(message,activator)
-   CFPython.Write("%d in the Jackpot, Play again?" %Slots.checkslot(),activator)
+   activator.Write(message,activator)
+   activator.Write("%d in the Jackpot, Play again?" %Slots.checkslot())
 else:
-   CFPython.Write("Sorry, you do not have enough money",activator)
+   activator.Write("Sorry, you do not have enough money")
