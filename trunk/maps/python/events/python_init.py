ffc8b07e280a914d47b4f98f9cdfdd25f6ecdadf
ryo_saeba <ryo_saeba@282e977c-c81d-0410-88c4-b93c2d0d6712>
2006-01-07 23:44:05 +0000

    Split event files, make subdirs.

--- trunk/maps/python/events/python_init.py
+++ trunk/maps/python/events/python_init.py
@@ -5,6 +5,10 @@ import sys
 print "Running python initialize script."
 sys.path.insert(0, os.path.join(Crossfire.DataDirectory(), Crossfire.MapDirectory(), 'python'))
 
-import CFGuilds
-print "Updating Guilds"
-CFGuilds.GuildUpdate()
+path = os.path.join(Crossfire.DataDirectory(), Crossfire.MapDirectory(), 'python/events/init')
+
+scripts = os.listdir(path)
+
+for script in scripts:
+	if (script.endswith('.py')):
+		execfile(os.path.join(path, script))
