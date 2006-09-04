03dbf4ae177e34fb57812f2e79f3f90d5f437759
mwedel <mwedel@282e977c-c81d-0410-88c4-b93c2d0d6712>
2006-09-04 06:42:12 +0000

    This change mainly adds support for the extended info messages for the gtkv2 client.  Note that unlike the gtkv1 client, the gtkv2 client doesn't in any way try to do pop up windows or fancy graphics - rather, the support is there just so it can do font changes, color changes, etc, within the normal text window. -- common/newclient.h: Sync it back up with server version.  Several new   MSG_ types added. gtk-v2/src/info.c: Redo info pane handling - move all the relevant variables    into a structure - this should make it easier to add more panes in the    future, but right now, it means an integer can be passed in to functions    to control what pane to draw in.  Add support for extended draw info -    notably, this means listing alternative fonts, and adding support    for different tags.  Add callsbacks for the different message types.  Add    code to process extended text message tags. gtk-v2/src/keys.c: Change the grab focus after user enters command to use    the treeview_look widget instead fo the textview - given that the textview    is now stored away in a structure, harder to get to that variable. MSW 2006-09-03

--- trunk/client/gtk-v2/src/info.c
+++ trunk/client/gtk-v2/src/info.c
@@ -40,41 +40,163 @@ char *rcsid_gtk2_info_c =
 
 #include "main.h"
 
-GtkWidget *textview_info1, *textview_info2, *sw1, *sw2;
-GtkTextBuffer *textbuf1, *textbuf2;
-GtkTextMark *textmark1, *textmark2;
+struct Info_Pane 
+{
+    GtkWidget	    *textview;
+    GtkWidget	    *scrolled_window;
+    GtkTextBuffer   *textbuffer;
+    GtkTextMark	    *textmark;
+    GtkAdjustment   *adjustment;
+    GtkTextTag	    *text_tags[NUM_COLORS];
+} info_pane[2];
 
-/* text_tag2 to represent it for for textbuffer 2 */
-GtkTextTag	*text_tag1[NUM_COLORS], *text_tag2[NUM_COLORS];
-GtkAdjustment   *adj1, *adj2;
+
+static void message_callback(int flag, int type, int subtype, char *message);
+
+/* Index into the different fonts.  font_families holds the names to
+ * use for the font with pango - pango seems to take a comma seperated
+ * listed.  What really should be done is let the use choose the fonts
+ * to use, since most systems do not have most of the font families
+ * listed (taken from gtk1 client)
+ */
+#define FONT_NORMAL	0
+#define FONT_ARCANE	1
+#define	FONT_STRANGE	2
+#define	FONT_FIXED	3
+#define FONT_HAND	4
+#define NUM_FONTS	5
+
+static char *font_families[NUM_FONTS] = {
+    "arial,bookman,agate", 
+    "cuneifontlight,linotext,blackforest,becker,arnoldboecklin,caligula,helvetica",
+    "annstone,shalomstick",
+    /* fixed doesn't scale, so put it at the end */
+    "courier,andale mono,urw bookman l,fixed",
+    "dobkinscript,coronetscript,muriel,genoa,parkavenue,rechtmanscript,luxi serif"
+};
+
+extern  char *colorname[NUM_COLORS];
 
 void info_init(GtkWidget *window_root)
 {
     int i;
     GtkTextIter end;
-    extern  char *colorname[NUM_COLORS];
 
-    textview_info1 = lookup_widget(window_root,"textview_info1");
-    textview_info2 = lookup_widget(window_root,"textview_info2");
-    sw1 = lookup_widget(window_root, "scrolledwindow_textview1");
-    sw2 = lookup_widget(window_root, "scrolledwindow_textview2");
-    gtk_text_view_set_wrap_mode(GTK_TEXT_VIEW(textview_info1), GTK_WRAP_WORD);
-    gtk_text_view_set_wrap_mode(GTK_TEXT_VIEW(textview_info2), GTK_WRAP_WORD);
-    textbuf1=gtk_text_view_get_buffer(GTK_TEXT_VIEW(textview_info1));
-    textbuf2=gtk_text_view_get_buffer(GTK_TEXT_VIEW(textview_info2));
-    adj1 = gtk_scrolled_window_get_vadjustment(GTK_SCROLLED_WINDOW(sw1));
-    adj2 = gtk_scrolled_window_get_vadjustment(GTK_SCROLLED_WINDOW(sw2));
+    info_pane[0].textview = lookup_widget(window_root,"textview_info1");
+    info_pane[1].textview = lookup_widget(window_root,"textview_info2");
+    info_pane[0].scrolled_window = lookup_widget(window_root, "scrolledwindow_textview1");
+    info_pane[1].scrolled_window = lookup_widget(window_root, "scrolledwindow_textview2");
+    gtk_text_view_set_wrap_mode(GTK_TEXT_VIEW(info_pane[0].textview), GTK_WRAP_WORD);
+    gtk_text_view_set_wrap_mode(GTK_TEXT_VIEW(info_pane[1].textview), GTK_WRAP_WORD);
+    info_pane[0].textbuffer=gtk_text_view_get_buffer(GTK_TEXT_VIEW(info_pane[0].textview));
+    info_pane[1].textbuffer=gtk_text_view_get_buffer(GTK_TEXT_VIEW(info_pane[1].textview));
+    info_pane[0].adjustment = gtk_scrolled_window_get_vadjustment(GTK_SCROLLED_WINDOW(info_pane[0].scrolled_window));
+    info_pane[1].adjustment = gtk_scrolled_window_get_vadjustment(GTK_SCROLLED_WINDOW(info_pane[1].scrolled_window));
 
-    gtk_text_buffer_get_end_iter(textbuf1, &end);
-    textmark1 =  gtk_text_buffer_create_mark(textbuf1, NULL, &end, FALSE);
+    gtk_text_buffer_get_end_iter(info_pane[0].textbuffer, &end);
+    info_pane[0].textmark =  gtk_text_buffer_create_mark(info_pane[0].textbuffer, NULL, &end, FALSE);
 
-    gtk_text_buffer_get_end_iter(textbuf2, &end);
-    textmark2 =  gtk_text_buffer_create_mark(textbuf2, NULL, &end, FALSE);
+    gtk_text_buffer_get_end_iter(info_pane[1].textbuffer, &end);
+    info_pane[1].textmark =  gtk_text_buffer_create_mark(info_pane[1].textbuffer, NULL, &end, FALSE);
 
     for (i=0; i<NUM_COLORS; i++) {
-	text_tag1[i] = gtk_text_buffer_create_tag(textbuf1, NULL, "foreground", colorname[i],NULL);
-	text_tag2[i] = gtk_text_buffer_create_tag(textbuf2, NULL, "foreground", colorname[i],NULL);
+	info_pane[0].text_tags[i] = gtk_text_buffer_create_tag(info_pane[0].textbuffer, NULL, "foreground", colorname[i],NULL);
+	info_pane[1].text_tags[i] = gtk_text_buffer_create_tag(info_pane[1].textbuffer, NULL, "foreground", colorname[i],NULL);
     }
+
+    setTextManager(MSG_TYPE_BOOK,message_callback);
+    setTextManager(MSG_TYPE_CARD,message_callback);
+    setTextManager(MSG_TYPE_PAPER,message_callback);
+    setTextManager(MSG_TYPE_SIGN,message_callback);
+    setTextManager(MSG_TYPE_MONUMENT,message_callback);
+    setTextManager(MSG_TYPE_SCRIPTED_DIALOG,message_callback);
+    setTextManager(MSG_TYPE_MOTD,message_callback);
+    setTextManager(MSG_TYPE_ADMIN,message_callback);
+    setTextManager(MSG_TYPE_SHOP,message_callback);
+    setTextManager(MSG_TYPE_COMMAND,message_callback);
+
+}
+
+
+static void add_to_textbuf(int pane, char *message, int weight, int style, int font, char *color)
+{
+    GtkTextIter end;
+    GdkRectangle rect;
+    int scroll_to_end=0;
+    gdouble scale=1.0;
+    GtkTextTag	    *tag;
+
+    gtk_text_view_get_visible_rect(GTK_TEXT_VIEW(info_pane[pane].textview), &rect);
+    if ((info_pane[pane].adjustment->value + rect.height) >= info_pane[pane].adjustment->upper ) scroll_to_end=1;
+
+    /* A bit of a hack, but relative to the proportional fonts, the fixed font is
+     * bigger, and thus needs to be scaled down some for things to look
+     * right
+     */
+    if (font == FONT_FIXED)  scale=0.9;
+
+    tag = gtk_text_buffer_create_tag(info_pane[pane].textbuffer, NULL,
+		"weight", weight,
+		"style", style,
+		"family", font_families[font],
+		"foreground", color,
+		"scale", scale,
+		 NULL);
+
+    gtk_text_buffer_get_end_iter(info_pane[pane].textbuffer, &end);
+
+    gtk_text_buffer_insert_with_tags(info_pane[pane].textbuffer, &end, message , strlen(message),
+	    tag, NULL);
+
+    if (scroll_to_end)
+	gtk_text_view_scroll_mark_onscreen(GTK_TEXT_VIEW(info_pane[pane].textview), info_pane[pane].textmark);
+
+}
+
+/* Unlike the gtk client, we don't do anything tricky like popups
+ * with different message types.
+ * However, we will choose different fonts, etc, based on this information -
+ * for this reason, we just use one callback, and change those minor
+ * things based on the callback.
+ * We also need to parse the data.
+ */
+static void message_callback(int orig_color, int type, int subtype, char *message) {
+    char *marker, *current, *original;
+    int	weight=PANGO_WEIGHT_NORMAL;
+    int style=PANGO_STYLE_NORMAL;
+    int	font=FONT_NORMAL;
+    char *color=colorname[orig_color];
+
+    current = strdup(message);
+    original = current;		/* Just so we know what to free */
+
+    while ((marker = strchr(current,'['))!= NULL) {
+	*marker = 0;
+	add_to_textbuf(0,current, weight, style, font, color);
+	current=marker+1;
+	if ((marker = strchr(current,']')) == NULL) {
+	    free(original);
+	    return;
+	}
+	*marker = 0;
+	if (!strcmp(current,"b"))	weight = PANGO_WEIGHT_BOLD;
+	else if (!strcmp(current,"/b"))	weight = PANGO_WEIGHT_NORMAL;
+	else if (!strcmp(current,"i"))	style = PANGO_STYLE_ITALIC;
+	else if (!strcmp(current,"/i"))	style = PANGO_STYLE_NORMAL;
+	else if (!strcmp(current,"fixed"))	font = FONT_FIXED;
+	else if (!strcmp(current,"arcane"))	font = FONT_ARCANE;
+	else if (!strcmp(current,"hand"))	font = FONT_HAND;
+	else if (!strcmp(current,"strange"))	font = FONT_STRANGE;
+	else if (!strcmp(current,"print"))	font = FONT_NORMAL;
+	else if (!strcmp(current,"/color"))	color = colorname[orig_color];
+	else if (!strncmp(current,"color=",6))	color = current + 6;
+	else LOG(LOG_INFO, "info.c::message_callback", "unidentified message: %s\n", current);
+	current = marker+1;
+
+    }
+    add_to_textbuf(0,current, weight, style, font, color);
+    add_to_textbuf(0, "\n", weight, style, font, color);
+    free(original);
 }
 
 /* draw_info adds a line to the info window.
@@ -102,36 +224,36 @@ void draw_info(const char *str, int color) {
      * etc)
      * We need to find out the position before putting in new text -
      * otherwise, that operation will mess up our position, and
-     * not giv us right info.
+     * not give us right info.
      */
-    gtk_text_view_get_visible_rect(GTK_TEXT_VIEW(textview_info1), &rect);
-    if ((adj1->value + rect.height) >= adj1->upper ) scroll_to_end=1;
+    gtk_text_view_get_visible_rect(GTK_TEXT_VIEW(info_pane[0].textview), &rect);
+    if ((info_pane[0].adjustment->value + rect.height) >= info_pane[0].adjustment->upper ) scroll_to_end=1;
 
     if (color == NDI_BLACK) {
-	gtk_text_buffer_get_end_iter(textbuf1, &end);
-	gtk_text_buffer_insert(textbuf1, &end, str , strlen(str));
-	gtk_text_buffer_insert(textbuf1, &end, "\n" , 1);
+	gtk_text_buffer_get_end_iter(info_pane[0].textbuffer, &end);
+	gtk_text_buffer_insert(info_pane[0].textbuffer, &end, str , strlen(str));
+	gtk_text_buffer_insert(info_pane[0].textbuffer, &end, "\n" , 1);
 
 	if (scroll_to_end)
-	    gtk_text_view_scroll_mark_onscreen(GTK_TEXT_VIEW(textview_info1), textmark1);
+	    gtk_text_view_scroll_mark_onscreen(GTK_TEXT_VIEW(info_pane[0].textview), info_pane[0].textmark);
     } else {
-	gtk_text_buffer_get_end_iter(textbuf1, &end);
-	gtk_text_buffer_insert_with_tags(textbuf1, &end, str , strlen(str), text_tag1[ncolor], NULL);
-	gtk_text_buffer_insert(textbuf1, &end, "\n" , 1);
+	gtk_text_buffer_get_end_iter(info_pane[0].textbuffer, &end);
+	gtk_text_buffer_insert_with_tags(info_pane[0].textbuffer, &end, str , strlen(str), info_pane[0].text_tags[ncolor], NULL);
+	gtk_text_buffer_insert(info_pane[0].textbuffer, &end, "\n" , 1);
 
 	if (scroll_to_end)
-	    gtk_text_view_scroll_mark_onscreen(GTK_TEXT_VIEW(textview_info1), textmark1);
+	    gtk_text_view_scroll_mark_onscreen(GTK_TEXT_VIEW(info_pane[0].textview), info_pane[0].textmark);
 
-	gtk_text_view_get_visible_rect(GTK_TEXT_VIEW(textview_info2), &rect);
-	if ((adj2->value + rect.height) >= adj2->upper ) scroll_to_end=1;
+	gtk_text_view_get_visible_rect(GTK_TEXT_VIEW(info_pane[1].textview), &rect);
+	if ((info_pane[1].adjustment->value + rect.height) >= info_pane[1].adjustment->upper ) scroll_to_end=1;
 	else scroll_to_end=0;
 
-	gtk_text_buffer_get_end_iter(textbuf2, &end);
-	gtk_text_buffer_insert_with_tags(textbuf2, &end, str , strlen(str), text_tag2[ncolor], NULL);
-	gtk_text_buffer_insert(textbuf2, &end, "\n" , 1);
+	gtk_text_buffer_get_end_iter(info_pane[1].textbuffer, &end);
+	gtk_text_buffer_insert_with_tags(info_pane[1].textbuffer, &end, str , strlen(str), info_pane[1].text_tags[ncolor], NULL);
+	gtk_text_buffer_insert(info_pane[1].textbuffer, &end, "\n" , 1);
 
 	if (scroll_to_end)
-	    gtk_text_view_scroll_mark_onscreen(GTK_TEXT_VIEW(textview_info2), textmark2);
+	    gtk_text_view_scroll_mark_onscreen(GTK_TEXT_VIEW(info_pane[1].textview), info_pane[1].textmark);
     }
 }
 
@@ -145,8 +267,8 @@ void draw_color_info(int colr, const char *buf){
  * support it.
  */
 void menu_clear() {
-    gtk_text_buffer_set_text(textbuf1, "", 0);
-    gtk_text_buffer_set_text(textbuf2, "", 0);
+    gtk_text_buffer_set_text(info_pane[0].textbuffer, "", 0);
+    gtk_text_buffer_set_text(info_pane[1].textbuffer, "", 0);
 }
 
 /* All the following are 'dummy' functions.  Basically, there are callbacks
