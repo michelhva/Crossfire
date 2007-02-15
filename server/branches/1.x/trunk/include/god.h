2756c066de5658fd707a01e4b746b8faa642fd85
mwedel <mwedel@282e977c-c81d-0410-88c4-b93c2d0d6712>
2007-02-15 07:36:08 +0000

    Delete server/branches/1.x/trunk - this isn't deleting the 1.x branch, but rather a copy of the server inside of the server area - no reason for it to be there, it (rightfully) isn't getting updated. MSW 2007-02-14

--- server/branches/1.x/trunk/include/god.h
+++
@@ -1,8 +0,0 @@
-typedef struct glnk { /* Used to link together the gods */
-  const char *name;       		/* name of this god */
-  struct archt *arch;      	/* pointer to the archetype of this god */
-  int id;               	/* id of the god */ 
-  const char *pantheon;      		/* the name of the group this god belongs to */
-  struct glnk *next;    
-} godlink; 
-
