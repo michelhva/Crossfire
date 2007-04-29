21f66b94b964d6597639df49b902cc092cf946d4
ryo_saeba <ryo_saeba@282e977c-c81d-0410-88c4-b93c2d0d6712>
2007-04-29 22:00:00 +0000

    Merge random map changes from trunk.

--- server/branches/1.x/random_maps/floor.c
+++ server/branches/1.x/random_maps/floor.c
@@ -31,8 +31,59 @@
 #include <random_map.h>
 #include <rproto.h>
 
-/*  make a map and layout the floor.  */
+/**
+ * Checks if the tile 'propagates' the floor.
+ * @param item
+ * tile to check.
+ * @return
+ * 1 if this tile should propagate, 0 else.
+ */
+static int can_propagate(char item) {
+    return (item == '\0' || item == '<' || item == '>') ? 1 : 0;
+}
 
+/**
+ * Put a floor at specified location, and then to adjacent tiles if applicable.
+ * @param map
+ * where to put the floor.
+ * @param layout
+ * layout that was used to generate the map.
+ * @param x
+ * @param y
+ * coordinates.
+ * @param floor_arch
+ * what floor to use.
+ */
+static void put_floor(mapstruct* map, char** layout, int x, int y, object* floor_arch) {
+    int dx, dy;
+    object* floor;
+
+    floor = arch_to_object(floor_arch->arch);
+    floor->x = x;
+    floor->y = y;
+    insert_ob_in_map(floor,map,floor,INS_NO_MERGE | INS_NO_WALK_ON);
+
+    for (dx = -1; dx < 2; dx++) {
+        for (dy = -1; dy < 2; dy++) {
+            if (GET_MAP_OB(map, x + dx, y + dy) == NULL && can_propagate(layout[x + dx][y + dy]))
+                put_floor(map, layout, x + dx, y + dy, floor_arch);
+        }
+    }
+}
+
+/**
+ * Creates the Crossfire mapstruct object from the layout, and adds the floor.
+ * @param layout
+ * generated layout.
+ * @param floorstyle
+ * floor style. Can be NULL, in which case a random one is chosen.
+ * @param RP
+ * parameters of the random map.
+ * @return
+ * Crossfire map.
+ * @todo
+ * use safe string functions.
+ */
 mapstruct *make_map_floor(char **layout, char *floorstyle,RMParms *RP) {
   char styledirname[256];
   char stylefilepath[256];
@@ -42,6 +93,7 @@ mapstruct *make_map_floor(char **layout, char *floorstyle,RMParms *RP) {
   
   /* allocate the map */
   newMap = get_empty_map(RP->Xsize,RP->Ysize);
+    int x, y;
 
   /* get the style map */
   sprintf(styledirname,"%s","/styles/floorstyles");
@@ -49,15 +101,25 @@ mapstruct *make_map_floor(char **layout, char *floorstyle,RMParms *RP) {
   style_map = find_style(styledirname,floorstyle,-1);
   if(style_map == 0) return newMap;
 
-  /* fill up the map with the given floor style */
-  if((the_floor=pick_random_object(style_map))!=NULL) {
-	 int i,j;
-	 for(i=0;i<RP->Xsize;i++)
-		for(j=0;j<RP->Ysize;j++) {
-		  object *thisfloor=arch_to_object(the_floor->arch);
-		  thisfloor->x = i; thisfloor->y = j;
-		  insert_ob_in_map(thisfloor,newMap,thisfloor,INS_NO_MERGE | INS_NO_WALK_ON);
-		}
-  }
-  return newMap;
+    if (RP->multiple_floors) {
+        for (x = 0; x < RP->Xsize; x++) {
+            for (y = 0; y < RP->Ysize; y++) {
+                if (GET_MAP_OB(newMap, x, y) == NULL && layout[x][y] == '\0')
+                    put_floor(newMap, layout, x, y, pick_random_object(style_map));
+            }
+        }
+    }
+
+    /* fill up the map with the given floor style */
+    if ((the_floor=pick_random_object(style_map))!=NULL) {
+        for(x=0;x<RP->Xsize;x++)
+            for(y=0;y<RP->Ysize;y++) {
+                if (GET_MAP_OB(newMap, x, y) != NULL)
+                    continue;
+                object *thisfloor = arch_to_object(the_floor->arch);
+                thisfloor->x = x; thisfloor->y = y;
+                insert_ob_in_map(thisfloor,newMap,thisfloor,INS_NO_MERGE | INS_NO_WALK_ON);
+            }
+    }
+    return newMap;
 }
