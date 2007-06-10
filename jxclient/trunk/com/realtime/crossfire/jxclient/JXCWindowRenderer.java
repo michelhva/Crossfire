41f368c10819ba284785406fc08a5fe80077c3ac
akirschbaum <akirschbaum@282e977c-c81d-0410-88c4-b93c2d0d6712>
2007-06-10 14:30:41 +0000

    Whitespace change.

--- jxclient/trunk/com/realtime/crossfire/jxclient/JXCWindowRenderer.java
+++ jxclient/trunk/com/realtime/crossfire/jxclient/JXCWindowRenderer.java
@@ -111,7 +111,8 @@ public class JXCWindowRenderer
         {
             forcePaint = false;
         }
-        else if (!needRedraw()) {
+        else if (!needRedraw())
+        {
             return;
         }
 
