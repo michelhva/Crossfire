8b35230dbcdbd22a8101d219692998365332e996
akirschbaum <akirschbaum@282e977c-c81d-0410-88c4-b93c2d0d6712>
2007-06-16 23:20:01 +0000

    Remove unused variable.

--- jxclient/trunk/com/realtime/crossfire/jxclient/ServerConnection.java
+++ jxclient/trunk/com/realtime/crossfire/jxclient/ServerConnection.java
@@ -111,7 +111,6 @@ public abstract class ServerConnection extends Thread
     public synchronized void readPacket() throws IOException, UnknownCommandException
     {
         int len = 0;
-        int i = 0;
 
         len = in.readUnsignedShort();
         byte[] data = new byte[len];
