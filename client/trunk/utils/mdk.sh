2135ca76aae86ceaf1f543d9c37290a0e1070148
akirschbaum <akirschbaum@282e977c-c81d-0410-88c4-b93c2d0d6712>
2007-06-27 20:13:28 +0000

    utils/mdk.sh: Make script work again.

--- client/trunk/utils/mdk.sh
+++ client/trunk/utils/mdk.sh
@@ -4,5 +4,5 @@ if [ -f def-keys.h ] ; then
   rm def-keys.h
 fi
 echo "const char *const def_keys[] = {" >def-keys.h
-cat def_keys | sed 's/^/"/' | sed 's/$/\\n",/' >>def-keys.h
+cat def-keys | sed 's/^/"/' | sed 's/$/\\n",/' >>def-keys.h
 echo "};" >>def-keys.h
