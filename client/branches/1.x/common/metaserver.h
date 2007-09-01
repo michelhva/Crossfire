3ea30fba90394a3dd425f97e6a167cb96891796e
mwedel <mwedel@282e977c-c81d-0410-88c4-b93c2d0d6712>
2007-09-01 05:51:19 +0000

    Make metaserver1 gather it's data in its own thread.  Brought back from trunk common/metaserver.c: Add ms1_is_running flag, re-work some of the metaserver1   logic so the function structure makes it easy to spawn the thread,   rename metaserver2_check_status() to just metaserver_check_status() gtk-v2/src/metaserver.c: Change call to metaserver_check_status(), resort   data after getting all info from sever. MSW 2007-08-31
    Add support for metaserver2 in client.  For the X11/C clients, they use basic
    interface (drawn in text window, select a number), so no changes to the GUI
    portions of those clients was done - gtk2 uses window, so some extra work
    there.  Brought back from trunk
    --
    */Makefile.in: rebuilt
    acinclude.m4: New file - added for LIBCURL_CHECK_CONFIG macro support.
    aclocal.m4: Rebuilt
    configure: Add --disable-metaserver2 option, check for Curl, pthread.  Make
      sure user either has curl installed or has used --disable-metaserver2
    configure, */Makefile.in:  Rebuilt with Curl support
    common/cconfig.h: Add note about METASERVER2 (controlled via configure)
    common/client.c: Add metaserver2 global
    common/client.h: Add metaserver2 global
    common/config.h.in: Rebuilt by autoheader.
    common/init.c: Add call to init_metaserver()
    common/metaserver.c: Support for metaserver2.  More fields in Meta_Info.
      Re-arrange file a little so logical functions are together.
    common/metaserver.h: New fields in Meta_Info, some renamed to keep
      it consistent accross all of metaserver2.
    common/proto.h: rebuilt
    gtk-v2/src/metaserver.c: Add support for threaded metaserver2 retrievals -
      need to make sure data is accessed in thread safe way, also need to 
      add check for status of retrieval thread.
    MSW 2007-08-31

--- client/branches/1.x/common/metaserver.h
+++ client/branches/1.x/common/metaserver.h
@@ -31,20 +31,49 @@
 #define MAX_METASERVER 100
 
 /* Various constants we use in the structure */
-#define MS_SMALL_BUF	20
-#define MS_LARGE_BUF	256
+#define MS_SMALL_BUF	60
+#define MS_LARGE_BUF	512
 
+/**
+ * Structure that contains data we get from metaservers
+ * This is used by both metaserver1 and metaserver2
+ * support - fields used by only one metaserver type
+ * or the other are noted with MS1 or MS2 comments.
+ * Note that the client doesn't necessary do anything with
+ * all of these fields, but might as well store them around
+ * just in case
+ */
 typedef struct Meta_Info {
-    char    ip_addr[MS_SMALL_BUF];
-    int	    idle_time;
-    char    hostname[MS_LARGE_BUF];
-    int	    players;
-    char    version[MS_SMALL_BUF];
-    char    comment[MS_LARGE_BUF];
+    char    ip_addr[MS_SMALL_BUF];	/* MS1 */
+    char    hostname[MS_LARGE_BUF];	/* MS1 & MS2 */
+    int	    port;			/* MS2 - port server is on */
+    char    html_comment[MS_LARGE_BUF];	/* MS2 */
+    char    text_comment[MS_LARGE_BUF];	/* MS1 & MS2 - for MS1, presumed */
+					/* all comments are text */
+    char    archbase[MS_SMALL_BUF];	/* MS2 */
+    char    mapbase[MS_SMALL_BUF];	/* MS2 */
+    char    codebase[MS_SMALL_BUF];	/* MS2 */
+    char    flags[MS_SMALL_BUF];	/* MS2 */
+    int	    num_players;		/* MS1 & MS2 */
+    uint32  in_bytes;			/* MS2 */
+    uint32  out_bytes;			/* MS2 */
+    int	    idle_time;			/* MS1 - for MS2, calculated from */
+					/* last_update value */
+    int	    uptime;			/* MS2 */
+    char    version[MS_SMALL_BUF];	/* MS1 & MS2 */
+    int	    sc_version;			/* MS2 */
+    int	    cs_version;			/* MS2 */
 } Meta_Info;
 
-
 extern Meta_Info *meta_servers;
+
+/* Before accessing the metaservers structure,
+ * a lock against this is needed
+ */
+extern pthread_mutex_t ms2_info_mutex;
+
+/* Needs to be here because gtk2 client needs to resort for example */
+static int meta_sort(Meta_Info *m1, Meta_Info *m2) { return strcasecmp(m1->hostname, m2->hostname); }
 
 extern int meta_numservers;
 
