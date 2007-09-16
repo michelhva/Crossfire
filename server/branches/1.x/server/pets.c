2e40829738a2e1f5df3f6da9b6df62ccfe02de46
akirschbaum <akirschbaum@282e977c-c81d-0410-88c4-b93c2d0d6712>
2007-09-16 19:52:54 +0000

    Remove trailing whitespace.

--- server/branches/1.x/server/pets.c
+++ server/branches/1.x/server/pets.c
@@ -78,7 +78,7 @@ object *get_pet_enemy(object * pet, rv_vector *rv){
 	if(tmp == owner && !QUERY_FLAG(pet,FLAG_CONFUSED)
 	    && QUERY_FLAG(pet,FLAG_FRIENDLY))
 	    /* without this check, you can actually get pets with
-	     * enemy set to owner! 
+	     * enemy set to owner!
 	     */
             pet->enemy = NULL;
 	else
@@ -103,7 +103,7 @@ object *get_pet_enemy(object * pet, rv_vector *rv){
 	}
     }
 
-    /* Since the pet has no existing enemy, look for anything nasty 
+    /* Since the pet has no existing enemy, look for anything nasty
      * around the owner that it should go and attack.
      */
     tmp3 = NULL;
@@ -119,10 +119,10 @@ object *get_pet_enemy(object * pet, rv_vector *rv){
 
 	    if (QUERY_FLAG(tmp2,FLAG_ALIVE) && ((
 				!QUERY_FLAG(tmp2, FLAG_FRIENDLY) &&
-				(tmp2->type != PLAYER)) || 
+				(tmp2->type != PLAYER)) ||
 			should_arena_attack(pet, owner, tmp2))
 		&& !QUERY_FLAG(tmp2,FLAG_UNAGGRESSIVE) &&
-		tmp2 != pet && tmp2 != owner && 
+		tmp2 != pet && tmp2 != owner &&
 		can_detect_enemy(pet, tmp2, rv)) {
 
 			if (!can_see_enemy(pet, tmp2)) {
@@ -186,11 +186,11 @@ object *get_pet_enemy(object * pet, rv_vector *rv){
 		for (tmp = get_map_ob(nm, x, y); tmp != NULL; tmp = tmp->above) {
 		    object *tmp2 = tmp->head == NULL?tmp:tmp->head;
 		    if (QUERY_FLAG(tmp2,FLAG_ALIVE) && ((
-					!QUERY_FLAG(tmp2, FLAG_FRIENDLY) && 
-					(tmp2->type != PLAYER)) || 
+					!QUERY_FLAG(tmp2, FLAG_FRIENDLY) &&
+					(tmp2->type != PLAYER)) ||
 				should_arena_attack(pet, owner, tmp2))
 			&& !QUERY_FLAG(tmp2,FLAG_UNAGGRESSIVE) &&
-			tmp2 != pet && tmp2 != owner && 
+			tmp2 != pet && tmp2 != owner &&
 			can_detect_enemy(pet, tmp2, rv)) {
 
 			    if (!can_see_enemy(pet, tmp2)) {
@@ -405,14 +405,14 @@ void pet_move(object * ob)
  ****************************************************************************/
 
 /* fix_summon_pet() - this makes multisquare/single square monsters
- * proper for map insertion. 
+ * proper for map insertion.
  * at is the archetype, op is the caster of the spell, dir is the
  * direction the monster should be placed in.
  * is_golem is to note that this is a golem spell.
  */
 static object *fix_summon_pet(archetype *at, object *op, int dir, int is_golem) {
     archetype *atmp;
-    object *tmp=NULL, *prev=NULL, *head=NULL; 
+    object *tmp=NULL, *prev=NULL, *head=NULL;
 
     for(atmp = at; atmp!=NULL; atmp = atmp->more) {
         tmp = arch_to_object(atmp);
@@ -517,9 +517,9 @@ void move_golem(object *op) {
 	return;
     }
 
-    /* Do golem attacks/movement for single & multisq golems. 
-     * Assuming here that op is the 'head' object. Pass only op to 
-     * move_ob (makes recursive calls to other parts) 
+    /* Do golem attacks/movement for single & multisq golems.
+     * Assuming here that op is the 'head' object. Pass only op to
+     * move_ob (makes recursive calls to other parts)
      * move_ob returns 0 if the creature was not able to move.
      */
     tag = op->count;
@@ -527,7 +527,7 @@ void move_golem(object *op) {
     if (was_destroyed (op, tag))
         return;
 
-    for(tmp=op;tmp;tmp=tmp->more) { 
+    for(tmp=op;tmp;tmp=tmp->more) {
 	sint16 x=tmp->x+freearr_x[op->direction],y=tmp->y+freearr_y[op->direction];
 	object *victim;
 	mapstruct *m;
@@ -538,7 +538,7 @@ void move_golem(object *op) {
 
 	if (mflags & P_OUT_OF_MAP) continue;
 
-	for(victim=get_map_ob(op->map,x,y);victim;victim=victim->above) 
+	for(victim=get_map_ob(op->map,x,y);victim;victim=victim->above)
 	    if(QUERY_FLAG(victim,FLAG_ALIVE)) break;
 
 	/* We used to call will_hit_self to make sure we don't
@@ -551,7 +551,7 @@ void move_golem(object *op) {
 	if(victim && victim!=op && victim->head!=op) {
 
 	    /* for golems with race fields, we don't attack
-	     * aligned races 
+	     * aligned races
 	     */
 
 	    if(victim->race && op->race && strstr(op->race,victim->race)) {
@@ -592,8 +592,8 @@ int summon_golem(object *op,object *caster,int dir,object *spob) {
     /* Because there can be different golem spells, player may want to
      * 'lose' their old golem.
      */
-    if(op->type==PLAYER && 
-       op->contr->ranges[range_golem]!=NULL && 
+    if(op->type==PLAYER &&
+       op->contr->ranges[range_golem]!=NULL &&
        op->contr->golem_count == op->contr->ranges[range_golem]->count) {
 	    new_draw_info(NDI_UNIQUE, 0, op, "You dismiss your existing golem.");
 	    remove_ob(op->contr->ranges[range_golem]);
@@ -604,7 +604,7 @@ int summon_golem(object *op,object *caster,int dir,object *spob) {
 
     if (spob->other_arch)
 	at = spob->other_arch;
-    else if (spob->race) { 
+    else if (spob->race) {
 	god = find_god(determine_god(caster));
 
 	if (!god) {
@@ -683,7 +683,7 @@ int summon_golem(object *op,object *caster,int dir,object *spob) {
      * this is a bit simplistic - we are basically just looking at how
      * often the sp doubles and use that as the ratio.
      */
-    tmp->stats.exp *= 1 + (MAX(spob->stats.maxgrace, spob->stats.sp) / 
+    tmp->stats.exp *= 1 + (MAX(spob->stats.maxgrace, spob->stats.sp) /
 			   caster_level(caster, spob));
     tmp->speed_left= 0;
     tmp->direction=dir;
@@ -736,20 +736,20 @@ static object *choose_cult_monster(object *pl, object *god, int summon_level) {
     /* Determine the number of races available */
     racenr=0;
     strcpy(buf,god->race);
-    race = strtok(buf,","); 
-    while(race) { 
-	racenr++; 
-	race = strtok(NULL,",");  
+    race = strtok(buf,",");
+    while(race) {
+	racenr++;
+	race = strtok(NULL,",");
      }
- 
+
     /* next, randomly select a race from the aligned_races string */
-    if(racenr>1) { 
+    if(racenr>1) {
 	racenr = rndm(0, racenr-1);
 	strcpy(buf,god->race);
         race = strtok(buf,",");
-        for(i=0;i<racenr;i++) 
+        for(i=0;i<racenr;i++)
 	     race = strtok(NULL,",");
-    } else 
+    } else
         race = god->race;
 
 
@@ -758,15 +758,15 @@ static object *choose_cult_monster(object *pl, object *god, int summon_level) {
      * checking is good for cases where the god archetypes mismatch the
      * race file
      */
-    if((list=find_racelink(race))==NULL) { 
+    if((list=find_racelink(race))==NULL) {
 	new_draw_info_format(NDI_UNIQUE, 0,pl,
 	    "The spell fails! %s's creatures are beyond the range of your summons",
 	     god->name);
 	LOG(llevDebug,"choose_cult_monster() requested non-existent aligned race!\n");
-	return 0; 
+	return 0;
     }
 
-    /* search for an apprplritate monster on this race list */ 
+    /* search for an apprplritate monster on this race list */
     mon_nr=0;
     for(tobl=list->member;tobl;tobl=tobl->next) {
 	otmp=tobl->ob;
@@ -806,7 +806,7 @@ int summon_object(object *op, object *caster, object *spell_ob, int dir, const c
 	int level = caster_level(caster, spell_ob);
 	treasure *tr, *lasttr=NULL;;
 
-	/* In old code, this was a very convuluted for statement,   
+	/* In old code, this was a very convuluted for statement,
 	 * with all the checks in the 'for' portion itself.  Much
 	 * more readable to break some of the conditions out.
 	 */
@@ -926,7 +926,7 @@ int summon_object(object *op, object *caster, object *spell_ob, int dir, const c
 			    SET_FLAG(tmp, FLAG_FRIENDLY);
 			    add_friendly_object(tmp);
 			    tmp->stats.exp = 0;
-			    if (spell_ob->attack_movement) 
+			    if (spell_ob->attack_movement)
 				tmp->attack_movement = spell_ob->attack_movement;
 			    if (get_owner(op))
 				set_owner(tmp, get_owner(op));
@@ -962,33 +962,33 @@ int summon_object(object *op, object *caster, object *spell_ob, int dir, const c
 is found */
 static object *get_real_owner(object *ob) {
 	object *realowner = ob;
-	
+
 	if (realowner == NULL) return NULL;
-	
+
 	while(get_owner(realowner) != NULL)
 	{
 		realowner = get_owner(realowner);
 	}
 	return realowner;
 }
-		    
+
 /* determines if checks so pets don't attack players or other pets should be
 overruled by the arena petmode */
 int should_arena_attack(object *pet,object *owner,object *target) {
 	object *rowner, *towner;
-	
+
 	/* exit if the target, pet, or owner is null. */
 	if ((target == NULL) || (pet == NULL) || (owner == NULL)) return 0;
-		
+
 	/* get the owners of itself and the target, this is to deal with pets of
 	pets */
 	rowner = get_real_owner(owner);
 	if (target->type != PLAYER) {
 		towner = get_real_owner(target);
 	} else {
-		towner = 0;	
+		towner = 0;
 	}
-	
+
 	/* if the pet has no owner, exit with error */
 	if (rowner == NULL) {
 		LOG(llevError,"Pet has no owner.\n");
@@ -1001,20 +1001,20 @@ int should_arena_attack(object *pet,object *owner,object *target) {
 		LOG(llevError,"Target is not a player but has no owner. We should not be here.\n");
 		return 0;
 	}
-	
+
 	/* make sure that the owner is a player */
 	if (rowner->type != PLAYER) return 0;
-	
+
 	/* abort if the petmode is not arena */
 	if (rowner->contr->petmode != pet_arena) return 0;
-	
+
 	/* abort if the pet, it's owner, or the target is not on battleground*/
-	if (!(op_on_battleground(pet, NULL, NULL) && 
-	     op_on_battleground(owner, NULL, NULL) && 
+	if (!(op_on_battleground(pet, NULL, NULL) &&
+	     op_on_battleground(owner, NULL, NULL) &&
 	     op_on_battleground(target, NULL, NULL)))
 	     return 0;
-	
-	/* if the target is a monster, make sure it's owner is not the same */	
+
+	/* if the target is a monster, make sure it's owner is not the same */
 	if ((target->type != PLAYER) && (rowner == towner)) return 0;
 
 	/* check if the target is a player which affects how it will handle
@@ -1032,6 +1032,6 @@ int should_arena_attack(object *pet,object *owner,object *target) {
 			if (rowner->contr->party == target->contr->party) return 0;
 		}
 	}
-	
+
 	return 1;
 }
