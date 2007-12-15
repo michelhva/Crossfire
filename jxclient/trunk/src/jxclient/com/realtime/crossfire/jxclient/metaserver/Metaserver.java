078bbed1a4ef3b05d9bb3ccdf03c8ea74535c75b
akirschbaum <akirschbaum@282e977c-c81d-0410-88c4-b93c2d0d6712>
2007-12-15 10:30:25 +0000

    Do not crash when metaserver is not available.

--- jxclient/trunk/src/jxclient/com/realtime/crossfire/jxclient/metaserver/Metaserver.java
+++ jxclient/trunk/src/jxclient/com/realtime/crossfire/jxclient/metaserver/Metaserver.java
@@ -22,8 +22,8 @@ package com.realtime.crossfire.jxclient.metaserver;
 import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.InputStreamReader;
+import java.io.IOException;
 import java.net.Socket;
-import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -117,14 +117,9 @@ public class Metaserver
                 socket.close();
             }
         }
-        catch (final UnknownHostException e)
+        catch (final IOException ex)
         {
-            // ignore
-        }
-        catch (final Exception e)
-        {
-            e.printStackTrace();
-            System.exit(0);
+            // ignore (but keep already parsed entries)
         }
         Collections.sort(metalist);
     }
