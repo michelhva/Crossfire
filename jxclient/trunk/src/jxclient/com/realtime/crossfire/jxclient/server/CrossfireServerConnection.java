c1f79c701661bbd4631ce9a0bc94e4af9fc53e23
akirschbaum <akirschbaum@282e977c-c81d-0410-88c4-b93c2d0d6712>
2008-01-12 20:42:11 +0000

    Correctly display skills after re-login.

--- jxclient/trunk/src/jxclient/com/realtime/crossfire/jxclient/server/CrossfireServerConnection.java
+++ jxclient/trunk/src/jxclient/com/realtime/crossfire/jxclient/server/CrossfireServerConnection.java
@@ -1034,6 +1034,7 @@ public class CrossfireServerConnection extends ServerConnection implements Faces
                     {
                         debugProtocolWrite("recv player tag="+tag+" weight="+weight+" face="+face+" name="+name+"\n");
                     }
+                    ItemsList.getStats().reset();
                     ItemsList.getItemsManager().setPlayer(new CfPlayer(tag, weight, Faces.getFace(face), name));
                 }
                 return;
