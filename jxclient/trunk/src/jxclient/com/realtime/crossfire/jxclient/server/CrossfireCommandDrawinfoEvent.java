2955620256ef6b2921c89206e7fd5e95b88fd689
akirschbaum <akirschbaum@282e977c-c81d-0410-88c4-b93c2d0d6712>
2008-03-19 19:17:53 +0000

    ragnorok: Make the "Chat" tab (somewhat) work on branch servers.

--- jxclient/trunk/src/jxclient/com/realtime/crossfire/jxclient/server/CrossfireCommandDrawinfoEvent.java
+++ jxclient/trunk/src/jxclient/com/realtime/crossfire/jxclient/server/CrossfireCommandDrawinfoEvent.java
@@ -32,11 +32,37 @@ public class CrossfireCommandDrawinfoEvent extends EventObject
     /** The serial version UID. */
     private static final long serialVersionUID = 1;
 
+    public static final int NDI_BLACK = 0;
+
+    public static final int NDI_WHITE = 1;
+
+    public static final int NDI_NAVY = 2;
+
+    public static final int NDI_RED = 3;
+
+    public static final int NDI_ORANGE = 4;
+
+    public static final int NDI_BLUE = 5;
+
+    public static final int NDI_DK_ORANGE = 6;
+
+    public static final int NDI_GREEN = 7;
+
+    public static final int NDI_LT_GREEN = 8;
+
+    public static final int NDI_GREY = 9;
+
+    public static final int NDI_BROWN = 10;
+
+    public static final int NDI_GOLD = 11;
+
+    public static final int NDI_TAN = 12;
+
     private final String mytext;
 
     private final int mytype;
 
-    public CrossfireCommandDrawinfoEvent(Object src, String msg, int type)
+    public CrossfireCommandDrawinfoEvent(final Object src, final String msg, final int type)
     {
         super(src);
         mytext = msg;
