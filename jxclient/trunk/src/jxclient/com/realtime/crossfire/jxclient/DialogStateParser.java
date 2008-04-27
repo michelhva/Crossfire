5724460bd57a10203cb176b8ec01f0d6c8aa1261
akirschbaum <akirschbaum@282e977c-c81d-0410-88c4-b93c2d0d6712>
2008-04-27 07:13:52 +0000

    Whitespace change.

--- jxclient/trunk/src/jxclient/com/realtime/crossfire/jxclient/DialogStateParser.java
+++ jxclient/trunk/src/jxclient/com/realtime/crossfire/jxclient/DialogStateParser.java
@@ -63,7 +63,7 @@ public class DialogStateParser
         final File dialogsFile;
         try
         {
-                dialogsFile = Filenames.getDialogsFile(skin.getSkinName());
+            dialogsFile = Filenames.getDialogsFile(skin.getSkinName());
         }
         catch (final IOException ex)
         {
