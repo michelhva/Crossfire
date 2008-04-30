27dad08fc8c8d4c09e4e1bb411a7485e0b389d28
akirschbaum <akirschbaum@282e977c-c81d-0410-88c4-b93c2d0d6712>
2008-04-30 16:44:46 +0000

    Fix syntax error.

--- jxclient/trunk/src/jxclient/com/realtime/crossfire/jxclient/gui/GUICommand.java
+++ jxclient/trunk/src/jxclient/com/realtime/crossfire/jxclient/gui/GUICommand.java
@@ -236,7 +236,7 @@ public class GUICommand
      */
     public static class ScrollNextParameter
     {
-        /** The element to activate.
+        /** The element to activate. */
         private final ActivatableGUIElement nextElement;
 
         /**
