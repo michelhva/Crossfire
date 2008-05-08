73eb78bbc1b220e870ec7280e74e24a0db169c95
akirschbaum <akirschbaum@282e977c-c81d-0410-88c4-b93c2d0d6712>
2008-05-08 20:14:40 +0000

    Move ItemsList.updateItem() to ItemsManager.

--- jxclient/trunk/src/jxclient/com/realtime/crossfire/jxclient/ItemsList.java
+++ jxclient/trunk/src/jxclient/com/realtime/crossfire/jxclient/ItemsList.java
@@ -19,8 +19,6 @@
 //
 package com.realtime.crossfire.jxclient;
 
-import com.realtime.crossfire.jxclient.faces.Faces;
-import com.realtime.crossfire.jxclient.items.CfItem;
 import com.realtime.crossfire.jxclient.items.ItemsManager;
 import com.realtime.crossfire.jxclient.spells.SpellsManager;
 import com.realtime.crossfire.jxclient.stats.Stats;
@@ -41,34 +39,6 @@ public class ItemsList
      * The stats information.
      */
     private final Stats stats = new Stats();
-
-    public void updateItem(final int flags, final int tag, final int valLocation, final int valFlags, final int valWeight, final int valFace, final String valName, final String valNamePl, final int valAnim, final int valAnimSpeed, final int valNrof)
-    {
-        final CfItem item = itemsManager.getItemOrPlayer(tag);
-        if (item == null)
-        {
-            System.err.println("updateItem: undefined item "+tag);
-            return;
-        }
-
-        final boolean wasopen = (flags&CfItem.UPD_FLAGS) != 0 && itemsManager.getCurrentFloorManager().getCurrentFloor() == item.getTag() && item.isOpen();
-        item.update(flags, valFlags, valWeight, Faces.getFace(valFace), valName, valNamePl, valAnim, valAnimSpeed, valNrof);
-        if ((flags&CfItem.UPD_LOCATION) != 0)
-        {
-            itemsManager.moveItem(item, valLocation);
-        }
-        if ((flags&CfItem.UPD_FLAGS) != 0)
-        {
-            if (item.isOpen())
-            {
-                itemsManager.getCurrentFloorManager().setCurrentFloor(item.getTag());
-            }
-            else if (wasopen)
-            {
-                itemsManager.getCurrentFloorManager().setCurrentFloor(0);
-            }
-        }
-    }
 
     public ItemsManager getItemsManager()
     {
