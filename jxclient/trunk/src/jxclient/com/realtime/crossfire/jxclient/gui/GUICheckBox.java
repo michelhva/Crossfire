4c6ef140743fa683f190f2b57dcb972d693859d4
akirschbaum <akirschbaum@282e977c-c81d-0410-88c4-b93c2d0d6712>
2008-06-22 15:24:26 +0000

    Add missing call to super.

--- jxclient/trunk/src/jxclient/com/realtime/crossfire/jxclient/gui/GUICheckBox.java
+++ jxclient/trunk/src/jxclient/com/realtime/crossfire/jxclient/gui/GUICheckBox.java
@@ -131,6 +131,7 @@ public class GUICheckBox extends ActivatableGUIElement
     /** {@inheritDoc} */
     @Override public void paintComponent(final Graphics g)
     {
+        super.paintComponent(g);
         final Graphics2D g2 = (Graphics2D)g;
         g2.setBackground(new Color(0, 0, 0, 0.0f));
         g.clearRect(0, 0, w, h);
