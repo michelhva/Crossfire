46061f70609ecaf7f79a29111c0a016c84706cc7
akirschbaum <akirschbaum@282e977c-c81d-0410-88c4-b93c2d0d6712>
2008-07-05 20:39:19 +0000

    Extrace command line parsing into separate class.

--- jxclient/trunk/src/jxclient/com/realtime/crossfire/jxclient/window/JXCWindow.java
+++ jxclient/trunk/src/jxclient/com/realtime/crossfire/jxclient/window/JXCWindow.java
@@ -37,7 +37,7 @@ import com.realtime.crossfire.jxclient.gui.textinput.GUIText;
 import com.realtime.crossfire.jxclient.items.CfPlayer;
 import com.realtime.crossfire.jxclient.items.ItemsManager;
 import com.realtime.crossfire.jxclient.items.PlayerListener;
-import com.realtime.crossfire.jxclient.main.JXClient;
+import com.realtime.crossfire.jxclient.main.Options;
 import com.realtime.crossfire.jxclient.mapupdater.CfMapUpdater;
 import com.realtime.crossfire.jxclient.metaserver.Metaserver;
 import com.realtime.crossfire.jxclient.server.CommandQueue;
@@ -765,13 +765,13 @@ public class JXCWindow extends JFrame implements KeyListener, CrossfireDrawextin
         addMouseMotionListener(mouseTracker);
         if (!setSkin(skinName))
         {
-            if (skinName.equals(JXClient.DEFAULT_SKIN))
+            if (skinName.equals(Options.DEFAULT_SKIN))
             {
                 System.exit(1);
             }
 
-            System.err.println("trying to load default skin "+JXClient.DEFAULT_SKIN);
-            if (!setSkin(JXClient.DEFAULT_SKIN))
+            System.err.println("trying to load default skin "+Options.DEFAULT_SKIN);
+            if (!setSkin(Options.DEFAULT_SKIN))
             {
                 System.exit(1);
                 throw new AssertionError();
