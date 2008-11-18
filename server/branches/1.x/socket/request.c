a87923c4ea2a9fb66b072b763aaaf9a0a0cdfd22
akirschbaum <akirschbaum@282e977c-c81d-0410-88c4-b93c2d0d6712>
2008-11-18 21:30:50 +0000

    Properly check "setup extendedTextInfos" settings.

--- server/branches/1.x/socket/request.c
+++ server/branches/1.x/socket/request.c
@@ -278,15 +278,18 @@ void set_up_cmd(char *buf, int len, socket_struct *ns)
 	    sprintf(tmpbuf,"%d", ns->ext_mapinfos);
 	    safe_strcat(cmdback, tmpbuf, &slen, HUGE_BUF);
 	} else if (!strcmp(cmd,"extendedTextInfos")) {
-	    /* Added by tchize
-	     * prepare to use the extended text commands
-	     * Client toggle this to non zero to get exttext
-	     */
-	    char tmpbuf[20];
+	    int has_readable_type;
 
-	    ns->has_readable_type = (atoi(param));
-	    sprintf(tmpbuf,"%d", ns->has_readable_type);
-	    safe_strcat(cmdback, tmpbuf, &slen, HUGE_BUF);
+	    has_readable_type = atoi(param);
+	    if (has_readable_type != 0 && has_readable_type != 1) {
+		safe_strcat(cmdback, "FALSE", &slen, HUGE_BUF);
+	    } else {
+		char tmpbuf[20];
+
+		ns->has_readable_type = has_readable_type;
+		snprintf(tmpbuf, sizeof(tmpbuf), "%d", has_readable_type);
+		safe_strcat(cmdback, tmpbuf, &slen, HUGE_BUF);
+	    }
 	} else if (!strcmp(cmd,"tick")) {
 	    ns->tick = atoi(param);
 	    safe_strcat(cmdback, param, &slen, HUGE_BUF);
