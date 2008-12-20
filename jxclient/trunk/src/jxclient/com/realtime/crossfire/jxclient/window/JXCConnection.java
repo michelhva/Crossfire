b3d0e2e4515a2a4977e58bbca84e33d2339b01dd
akirschbaum <akirschbaum@282e977c-c81d-0410-88c4-b93c2d0d6712>
2008-12-20 22:52:54 +0000

    Extract connection handling code from JXCWindow into JXCConnection.

---
+++ jxclient/trunk/src/jxclient/com/realtime/crossfire/jxclient/window/JXCConnection.java
@@ -0,0 +1,174 @@
+package com.realtime.crossfire.jxclient.window;
+
+import com.realtime.crossfire.jxclient.server.Pickup;
+import com.realtime.crossfire.jxclient.settings.Settings;
+import com.realtime.crossfire.jxclient.util.NumberParser;
+import java.awt.Frame;
+
+public class JXCConnection
+{
+    /**
+     * The prefix for the window title.
+     */
+    private static final String TITLE_PREFIX = "jxclient";
+
+    /**
+     * The {@link KeybindingsManager} to update.
+     */
+    private final KeybindingsManager keybindingsManager;
+
+    /**
+     * The settings instance to use.
+     */
+    private final Settings settings;
+
+    /**
+     * The {@link Frame} for updating the title.
+     */
+    private final Frame frame;
+
+    /**
+     * The {@link Pickup} instance to update.
+     */
+    private final Pickup characterPickup;
+
+    /**
+     * The currently connected server. Set to <code>null</code> if unconnected.
+     */
+    private String hostname = null;
+
+    /**
+     * The currently connected port. Only valid if {@link #hostname} is set.
+     */
+    private int port = 0;
+
+    /**
+     * The currently logged in character. Set to <code>null</code> if not
+     * logged in.
+     */
+    private String character = null;
+
+    /**
+     * Creates a new instance.
+     * @param keybindingsManager the keybindings manager to update
+     * @param settings the settings instance to use
+     * @param frame the frame instance for updating the title
+     * @param characterPickup the character pickup instance to update
+     */
+    public JXCConnection(final KeybindingsManager keybindingsManager, final Settings settings, final Frame frame, final Pickup characterPickup)
+    {
+        this.keybindingsManager = keybindingsManager;
+        this.settings = settings;
+        this.frame = frame;
+        this.characterPickup = characterPickup;
+        updateTitle();
+    }
+
+    /**
+     * Returns the currently connected server.
+     * @return the server or <code>null</code> if unconnected
+     */
+    public String getHostname()
+    {
+        return hostname;
+    }
+
+    /**
+     * Returns the currently connected port.
+     * @return the port or <code>0</code> if unconnected
+     */
+    public int getPort()
+    {
+        return port;
+    }
+
+    /**
+     * Update the active character name.
+     *
+     * @param character The active character; <code>null</code> if not logged
+     * in.
+     */
+    public void setCharacter(final String character)
+    {
+        if (this.character == null ? character == null : this.character.equals(character))
+        {
+            return;
+        }
+
+        keybindingsManager.unloadPerCharacterBindings();
+
+        if (hostname != null && this.character != null)
+        {
+            final long pickupMode = characterPickup.getPickupMode();
+            if (pickupMode != Pickup.PU_NOTHING)
+            {
+                settings.putLong("pickup_"+hostname+"_"+this.character, pickupMode);
+            }
+            else
+            {
+                settings.remove("pickup_"+hostname+"_"+this.character);
+            }
+        }
+
+        this.character = character;
+        updateTitle();
+
+        if (hostname != null && character != null)
+        {
+            keybindingsManager.loadPerCharacterBindings(hostname, character);
+            characterPickup.setPickupMode(settings.getLong("pickup_"+hostname+"_"+character, Pickup.PU_NOTHING));
+        }
+    }
+
+    /**
+     * Update the window title to reflect the current connection state.
+     */
+    private void updateTitle()
+    {
+        if (hostname == null)
+        {
+            frame.setTitle(TITLE_PREFIX);
+        }
+        else if (character == null)
+        {
+            frame.setTitle(TITLE_PREFIX+" - "+hostname);
+        }
+        else
+        {
+            frame.setTitle(TITLE_PREFIX+" - "+hostname+" - "+character);
+        }
+    }
+
+    /**
+     * Update information about the connected host.
+     *
+     * @param serverInfo The hostname; <code>null</code> if not connected.
+     */
+    public void setHost(final String serverInfo)
+    {
+        final String newHostname;
+        final int newPort;
+        if (serverInfo == null)
+        {
+            newHostname = null;
+            newPort = 0;
+        }
+        else
+        {
+            final String[] tmp = serverInfo.split(":", 2);
+            newHostname = tmp[0];
+            newPort = tmp.length < 2 ? 13327 : NumberParser.parseInt(tmp[1], 13327, 1, 65535);
+        }
+
+        if ((hostname == null ? newHostname == null : hostname.equals(newHostname))
+        && port == newPort)
+        {
+            return;
+        }
+
+        setCharacter(null);
+        hostname = newHostname;
+        port = newPort;
+        updateTitle();
+    }
+}
