fc33d309bca5e9b4fef1d2984007da8469d3f889
akirschbaum <akirschbaum@282e977c-c81d-0410-88c4-b93c2d0d6712>
2009-02-06 21:39:55 +0000

    Extract GuiElementParser from JXCSkinLoader.

---
+++ jxclient/trunk/src/jxclient/com/realtime/crossfire/jxclient/skin/GuiElementParser.java
@@ -0,0 +1,79 @@
+//
+// This file is part of JXClient, the Fullscreen Java Crossfire Client.
+//
+//    JXClient is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    JXClient is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with JXClient; if not, write to the Free Software
+//    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+//
+// JXClient is (C)2005 by Yann Chachkoff.
+//
+package com.realtime.crossfire.jxclient.skin;
+
+import com.realtime.crossfire.jxclient.gui.AbstractLabel;
+import com.realtime.crossfire.jxclient.gui.GUIElement;
+import com.realtime.crossfire.jxclient.gui.textinput.GUIText;
+
+/**
+ * Creates gui element instances from string representations.
+ * @author Andreas Kirschbaum
+ */
+public class GuiElementParser
+{
+    /**
+     * All defined gui elements.
+     */
+    private final JXCSkinCache<GUIElement> definedGUIElements;
+
+    /**
+     * Creates a new instance.
+     * @param definedGUIElements all defined gui elements
+     */
+    public GuiElementParser(final JXCSkinCache<GUIElement> definedGUIElements)
+    {
+        this.definedGUIElements = definedGUIElements;
+    }
+
+    /**
+     * Returns a {@link GUIText} by element name.
+     * @param name the element name
+     * @return the <code>GUIText</code> element
+     * @throws JXCSkinException if the element name is undefined
+     */
+    public GUIText lookupTextElement(final String name) throws JXCSkinException
+    {
+        final GUIElement element = definedGUIElements.lookup(name);
+        if (!(element instanceof GUIText))
+        {
+            throw new JXCSkinException("element "+name+" is not a text field");
+        }
+
+        return (GUIText)element;
+    }
+
+    /**
+     * Returns a {@link AbstractLabel} by element name.
+     * @param name the element name
+     * @return the <code>AbstractLabel</code> element
+     * @throws JXCSkinException if the element name is undefined
+     */
+    public AbstractLabel lookupLabelElement(final String name) throws JXCSkinException
+    {
+        final GUIElement element = definedGUIElements.lookup(name);
+        if (!(element instanceof AbstractLabel))
+        {
+            throw new JXCSkinException("element "+name+" is not a label");
+        }
+
+        return (AbstractLabel)element;
+    }
+}
