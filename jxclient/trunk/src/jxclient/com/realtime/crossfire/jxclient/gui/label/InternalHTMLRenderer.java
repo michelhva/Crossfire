7907f684312bac4c57488a8f90690b308a1566c4
akirschbaum <akirschbaum@282e977c-c81d-0410-88c4-b93c2d0d6712>
2009-02-15 09:48:13 +0000

    Move InternalHTMLRenderer to label package.

--- jxclient/trunk/src/jxclient/com/realtime/crossfire/jxclient/gui/label/InternalHTMLRenderer.java
+++ jxclient/trunk/src/jxclient/com/realtime/crossfire/jxclient/gui/label/InternalHTMLRenderer.java
@@ -17,7 +17,7 @@
 //
 // JXClient is (C)2005 by Yann Chachkoff.
 //
-package com.realtime.crossfire.jxclient.gui;
+package com.realtime.crossfire.jxclient.gui.label;
 
 import java.awt.Color;
 import java.awt.Font;
