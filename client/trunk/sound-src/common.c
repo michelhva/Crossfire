1a8e5cefaf11e5027c48596e3453cc93582ade44
kbulgrien <kbulgrien@282e977c-c81d-0410-88c4-b93c2d0d6712>
2009-08-20 11:56:10 +0000

    - Add doxygen @file and in the case of common.c, add a header not present   that is commonly used in all other source files.

--- client/trunk/sound-src/common.c
+++ client/trunk/sound-src/common.c
@@ -1,3 +1,32 @@
+static char *rcsid_sound_src_common_c =
+    "$Id$";
+/*
+    Crossfire client, a client program for the crossfire program.
+
+    Copyright (C) 2001 Mark Wedel & Crossfire Development Team
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+    The author can be reached via e-mail to crossfire-devel@real-time.com
+*/
+
+/**
+ * @file sound-src/common.c
+ *
+ */
+
 /*
  * A replacement of strdup(), since it's not defined at some
  * unix variants.
