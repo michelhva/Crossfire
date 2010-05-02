a28604f813070d520d18694521c09382ba749113
akirschbaum <akirschbaum@282e977c-c81d-0410-88c4-b93c2d0d6712>
2010-05-02 10:13:02 +0000

    Remove unused parameter.

--- jxclient/trunk/src/jxclient/com/realtime/crossfire/jxclient/gui/label/GUILabelFailure.java
+++ jxclient/trunk/src/jxclient/com/realtime/crossfire/jxclient/gui/label/GUILabelFailure.java
@@ -21,7 +21,6 @@
 package com.realtime.crossfire.jxclient.gui.label;
 
 import com.realtime.crossfire.jxclient.gui.gui.GUIElementListener;
-import com.realtime.crossfire.jxclient.gui.gui.JXCWindowRenderer;
 import com.realtime.crossfire.jxclient.gui.gui.TooltipManager;
 import com.realtime.crossfire.jxclient.server.crossfire.CrossfireFailureListener;
 import com.realtime.crossfire.jxclient.server.crossfire.CrossfireServerConnection;
@@ -70,12 +69,11 @@ public class GUILabelFailure extends GUIMultiLineLabel {
      * @param name The name of this element.
      * @param extent the extent of this element
      * @param crossfireServerConnection the connection instance
-     * @param windowRenderer the window renderer this element belongs to
      * @param font The font to use.
      * @param color The color to use.
      * @param backgroundColor The background color.
      */
-    public GUILabelFailure(@NotNull final TooltipManager tooltipManager, @NotNull final GUIElementListener elementListener, @NotNull final String name, @NotNull final Extent extent, @NotNull final CrossfireServerConnection crossfireServerConnection, @NotNull final JXCWindowRenderer windowRenderer, @NotNull final Font font, @NotNull final Color color, @NotNull final Color backgroundColor) {
+    public GUILabelFailure(@NotNull final TooltipManager tooltipManager, @NotNull final GUIElementListener elementListener, @NotNull final String name, @NotNull final Extent extent, @NotNull final CrossfireServerConnection crossfireServerConnection, @NotNull final Font font, @NotNull final Color color, @NotNull final Color backgroundColor) {
         super(tooltipManager, elementListener, name, extent, null, font, color, backgroundColor, Alignment.LEFT, "");
         this.crossfireServerConnection = crossfireServerConnection;
         this.crossfireServerConnection.addCrossfireFailureListener(crossfireFailureListener);
