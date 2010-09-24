e825df3a7f290c96d0dc7c69cf6bb7d9dc590151
kbulgrien <kbulgrien@282e977c-c81d-0410-88c4-b93c2d0d6712>
2010-09-24 04:33:27 +0000

    - Modify the script to produce the def_sounds[] array in a .c file instead   of a .h file so that the data can be compiled to a .o for use in multiple   .c files that make up a sound binary. - The script now also produces a .h file that contains and extern prototype   for def_sounds[].

--- client/trunk/utils/deftoheader.pl
+++ client/trunk/utils/deftoheader.pl
@@ -16,13 +16,21 @@ if ($#ARGV!=2) {
 open(INFILE, "<$ARGV[0]") || die("can not open $ARGV[0]\n");
 open(OUTFILE, ">$ARGV[1]") || die("can not open $ARGV[1]\n");
 
-print OUTFILE "char *$ARGV[2]\[\] = {\n";
-while (<INFILE>) {
-	chomp;
-	s/\"/\\\"/g;
-	print OUTFILE "\"$_\\n\",\n";
+$LINES += tr/\n/\n/ while sysread(INFILE, $_, 2 ** 16);
+
+if ( $ARGV[1] =~ m/.h$/) {
+    print OUTFILE "extern char *$ARGV[2]\[" . $LINES . "\];\n";
+} else {
+    seek(INFILE, 0, 0);
+    print OUTFILE "char *$ARGV[2]\[" . $LINES . "\] = {\n";
+    while (<INFILE>) {
+        chomp;
+        s/\"/\\\"/g;
+        print OUTFILE "\"$_\\n\",\n";
+    }
+    print OUTFILE "};\n";
 }
-print OUTFILE "};\n";
-print "$. lines processed\n";
+print $LINES . " lines processed\n";
 close(INFILE);
 close(OUTFILE);
+
