fcf160b75167faa7feb863ee46156407f6f2a72b
kbulgrien <kbulgrien@282e977c-c81d-0410-88c4-b93c2d0d6712>
2010-10-14 04:01:47 +0000

    common.h - Add #defines for commonly used sound server data directories and externs for   the pointer variables they will ultimately be used with. - Add a helpful #define SOUND_DEBUG in this common file. - Add an extern to the client replace_chars_with_string() function so that the   sound server can transparently support backslash delimited path strings. sndproto.h - Rebuild to support the new stdin handler and the sound number and type   translator functions. common.c - Instead of allocating memory for paths each time a file is referenced, set   up pointers for each path that is used more than once, and allocate the   memory for them one time via init_sounds(). - Remove function prototypes as common.h is the correct place to put them. - Update a comment relating to play_sound()'s sound_number parameter. - Document the rationale behind use of MAXSOCKBUF for a file path buffer size. - Remove individualized setup of paths at the point-of-use in init_sounds(),   write_settings(), and read_settings(). - To smooth the transition from sound to sound2 command support by cfsndserv,   or, rather, to temporarily limit the scope of the rewrite and speed time to   initial functionality testing, create a stub sound_to_soundnum() translator   to take legacy sound names and map them to sound numbers so all legacy   sound drivers do not have to be re-written all at once.  For this commit,   the function is a stub, so sound effects are basically disabled, but the   code compiles cleanly. - As with conversion of sound name to sound number, create type_to_soundtype()   to convert sound2 types to sound types so that legacy sound effects can be   played with minimal rewrite.  This conversion layer is hopefully temporary,   but should allow minimally functional sound support much earlier as it   prevents a need to immediately rewrite the drivers for all supported sound   systems. - Rename SoundCmd() to StdinCmd() and rewrite it to parse both sound2 and   music commands coming from the client over stdin.  The format of commands   from the client is completely changed to support all of the parameters that   the sound2 protocol defines, and requires string arguments to be quoted to   make it easier to manage names with a variety of characters including white   space.  StdinCmd() does basic parameter sanity checks on the data so it can   discard invalid commands.   StdinCmd() detects the type of command by the   location of the quoted strings in the client commands. alsa9.c common.c cfsndserv.c - Add client includes to the sound server to allow more commonality between   the sound server and the client. alsa9.c cfsndserv.c - Update the sound server notice that identifies how it is compiled and add   a statement summarizing what is supported and not supported. - Standardize SOUND_DEBUG announcements when entering primary sound support   functions. - Add a play_music() stub function to all legacy sound systems (ALSA, ALSA9,   Sun, SGI, and OSS) that does nothing when they are selected, and add a very   basic (but functional) play_music() function that plays music if SDL_SOUND   is compiled in.  Having play_music() available for all sound systems lets   StdinCmd() remain the same no matter which sound system is built. - Rename calls to SoundCmd() to StdinCmd(). cfsndserv.c - Move the helpful #define SOUND_DEBUG to common.h. - Relocate all SDL_mixer sound support functions into the same #ifdef and all   the legacy functions into the same #else to put the related functions in   closer proximity with each other.

--- client/trunk/sound-src/common.c
+++ client/trunk/sound-src/common.c
@@ -37,6 +37,10 @@ const char * rcsid_sound_src_common_c =
 
 #include <ctype.h>
 
+#include "client-types.h"
+#include "newclient.h"
+#include "client.h"
+
 #include "def_sounds.h"
 #include "common.h"
 
@@ -44,6 +48,11 @@ Sound_Info normal_sounds[MAX_SOUNDS];
 Sound_Info spell_sounds[MAX_SOUNDS];
 Sound_Info default_normal;
 Sound_Info default_spell;
+
+char *client_sounds_path = NULL;        /* Client sound file folder         */
+char *user_sounds_path   = NULL;        /* User sound file folder           */
+char *user_sounds_file   = NULL;        /* User sound mappings              */
+char *user_config_file   = NULL;        /* User sndconfig file.             */
 
 char *buffers = NULL;
 
@@ -56,21 +65,6 @@ int sample_size = 0;
 int frequency = 0;
 int sign = 0;
 int zerolevel = 0;
-
-/**
- *
- * @return Zero if audio initialized successfully, otherwise -1.
- */
-extern int init_audio(void);
-
-/**
- *
- * @param soundnum
- * @param soundtype
- * @param x
- * @param y
- */
-void play_sound(int soundnum, int soundtype, int x, int y);
 
 /**
  * Parse a line from the sound file.  This is a little ugly because static
@@ -93,8 +87,9 @@ void play_sound(int soundnum, int soundtype, int x, int y);
  *
  * sound_number is a zero-based integer that identifies the sound.  If it is
  *     omitted, the last sound number used is incremented by one.  The server
- *     communicates what sound to play via the sound number, so the order is
- *     not arbitrary.
+ *     used to communicate what sound to play via the sound number, so the
+ *     order was not arbitrary, but the number and order is now obsolete from
+ *     a server perspective.
  *
  * @param line   A line of data from the .crossfire/sounds configuration file.
  *               Note that this data may be modified by parse_sound_line().
@@ -318,13 +313,24 @@ static void parse_sound_line(char *line, int lineno) {
  * Opens the audio device, allocates buffers, and reads any configuration
  * files that need to be.
  *
+ * http://en.wikipedia.org/wiki/Comparison_of_file_systems seems to show that
+ * 255 characters is the maximum file name length for most file systems.  The
+ * same page notes that many file systems have no defined limit to directory
+ * depth.  Some operating environments have a maximum that is quite large -
+ * for example, Windows NT can handle paths up to 32,767 bytes.  This data,
+ * along with the fact that the server Music command from the server has no
+ * inherent limitation (other than MAXSOCKBUF), is why MAXSOCKBUF is chosen
+ * for the maximum size of the path buffer.  MAXSOCKBUF is rather large, but
+ * the buffer size is practically only temporarily allocated (via the stack).
+ *
  * @return Zero on success and on failure, the calling function will likely
  *         disable sound support/requests from the server.
  */
 int init_sounds(void) {
-    int   i;
     FILE *fp;
-    char  path[256], buf[512];
+    char  path[MAXSOCKBUF];
+    char  buf[512];
+    int   i;
 
 #ifdef SOUND_DEBUG
     fprintf( stderr, "Settings: bits: %i, ", settings.bit8 ? 8 : 16);
@@ -333,6 +339,50 @@ int init_sounds(void) {
     fprintf( stderr, "frequency: %i, ", settings.frequency);
     fprintf( stderr, "device: %s\n", settings.audiodev);
 #endif
+
+    /*
+     * Force the last char of the buffer to null in case strn* cuts off the
+     * terminating null while copying file information.
+     */
+    path[sizeof(path) - 1] = '\0';
+    /*
+     * Initialize paths to various sound system resources.  Bail if any of
+     * the buffer allocations fail.
+     */
+    strncpy(path, getenv("HOME"), sizeof(path) - 1);
+    strncat(path, USER_CONFIG_FILE, sizeof(path) - 1);
+    CONVERT_FILESPEC_TO_OS_FORMAT(path);
+    user_config_file = (char *) malloc(strlen(path));
+    if (user_config_file)
+        strcpy(user_config_file, path);
+    else
+        return -1;
+
+    strncpy(path, getenv("HOME"), sizeof(path) - 1);
+    strncat(path, USER_SOUNDS_FILE, sizeof(path) - 1);
+    CONVERT_FILESPEC_TO_OS_FORMAT(path);
+    user_sounds_file = (char *) malloc(strlen(path));
+    if (user_sounds_file)
+        strcpy(user_sounds_file, path);
+    else
+        return -1;
+
+    strncpy(path, getenv("HOME"), sizeof(path) - 1);
+    strncat(path, USER_SOUNDS_PATH, sizeof(path) - 1);
+    CONVERT_FILESPEC_TO_OS_FORMAT(path);
+    user_sounds_path = (char *) malloc(strlen(path));
+    if (user_sounds_path)
+        strcpy(user_sounds_path, path);
+    else
+        return -1;
+
+    strncpy(path, CLIENT_SOUNDS_PATH, sizeof(path) - 1);
+    CONVERT_FILESPEC_TO_OS_FORMAT(path);
+    client_sounds_path = (char *) malloc(strlen(path));
+    if (client_sounds_path)
+        strcpy(client_sounds_path, path);
+    else
+        return -1;
 
     buffers = (char *) malloc(settings.buffers * settings.buflen);
     if (!buffers)
@@ -370,11 +420,11 @@ int init_sounds(void) {
     default_normal.filename = NULL;
     default_spell.filename = NULL;
 
-    sprintf(path, "%s/.crossfire/sounds", getenv("HOME"));
     i = 0;
-    if (!(fp = fopen(path, "r"))) {
+    if (!(fp = fopen(user_sounds_file, "r"))) {
         fprintf(stderr,
-            "Unable to open %s - will use built in defaults\n", path);
+            "Unable to open %s - using built-in defaults\n",
+                user_sounds_file);
         for (; i < sizeof(def_sounds) / sizeof(char*); i++) {
             strcpy(buf, def_sounds[i]);
             parse_sound_line(buf, i);
@@ -400,32 +450,213 @@ int init_sounds(void) {
 }
 
 /**
- * Handle sound-related commands that are received from stdin.  Sound commands
- * consist of four whitespace delimited values:  sound_number, sound_type, x,
- * and y.  The sound_number and sound_type identify which sound should play,
- * while x and y represents the map coordinate difference between the sound
- * source and the player/character.
+ * Convert a sound name to a sound number to help with the transition of the
+ * sound server from sound support to sound2 capability.  This is not an end
+ * solution, but one that gets the sound server working a little bit until a
+ * better one can be implemented.
  *
- * @param data A text buffer that (hopefully) contains a sound command.
- * @param len  The length of the text data in the sound command buffer.
- * @return     0 if the buffer is a well-formed sound command, otherwise -1.
+ * @param name
+ * @param type
  */
-int SoundCmd(unsigned char *data, int len) {
-    int x, y, num, type;
-    int i;
+int sound_to_soundnum(const char *name, uint8 type) {
 
-    i = sscanf((char *)data,"%x %x %x %x", &num, &type, &x, &y);
-    if (i != 4){
-        fprintf(stderr, "Wrong input!\n");
-        return -1;
-    }
+    fprintf(stderr, "name=%s type=%d\n", name, type);
+    /**
+     * @todo Implement conversion to legacy soundnum.
+     * @todo Replace conversion to legacy soundnum.
+     */
+    return 1;
+}
+
+/**
+ * Convert a sound type to legacy type to help with the transition of the
+ * sound server from sound support to sound2 capability.  This is not an end
+ * solution, but one that gets the sound server working a little bit until a
+ * better one can be implemented.  Basically, all types except 2 get changed
+ * to 1.
+ *
+ * @param type
+ */
+int type_to_soundtype(uint8 type) {
 
 #ifdef SOUND_DEBUG
     fprintf(stderr,
-        "Playing sound %d (type %d), offset %d, %d\n", num, type, x ,y);
+        "Converted type %d to legacy type %d.\n", type, (type == 2) ? 2 : 1);
 #endif
+    /**
+     * @todo Replace conversion to legacy soundtype.
+     */
+    return (type == 2) ? 2 : 1;
+}
 
-    play_sound(num, type, x, y);
+/**
+ * Preliminary handler for Crossfire server sound2 and music commands that are
+ * received from the client via stdin.
+ *
+ * The sound player differentiates sound2 and music commands by looking at the
+ * first parameter that comes in.  Music commands consist of a single, quoted
+ * string that identifies the music to play, while a sound effect command has
+ * various numeric parameters followed by strings that identify what to play.
+ *
+ * Sound2 data consists of whitespace delimited values:  x, y, dir, vol, type,
+ * sound, and source.  Type, sound, and source define what to play, while the
+ * other parameters may be used to figure out how to play it.  x and y are
+ * offsets from the player to identify where the sound originated. dir can be
+ * set to indicate a direction that the source is travelling in. vol is an
+ * attenuation factor (0-100) that may be applied to the sound volume to make
+ * it possible, for example, to give map-designers the ability to suggest
+ * relative loudness of sounds in the environment.
+ *
+ * FIXME: This is a work-in-progress.  The sound2 was put into the server
+ * without a plan to fix the clients.  cfsndserv is basically made obsolete by
+ * sound2.  The basic fix resurrects some sound support but does not fully
+ * implement the features sound2 is supposed to provide.
+ *
+ * @param data A text buffer that (hopefully) has a sound or music command.
+ * @param len  The length of the text data in the command buffer.
+ * @return     0 if the buffer contains a well-formed command, otherwise -1.
+ */
+int StdinCmd(char *data, int len) {
+    char* dptr;                         /* Pointer used when parsing data */
+    char* fptr;
+    char* sound = NULL;                 /* Points to a sound or music name */
+    char* source = NULL;
+    char  soundfile[MAXSOCKBUF];
+    int   sourcelen;
+    int   soundlen;
+    int   spacelen;
+    int   type = 0;
+    int   dir = 0;
+    int   vol = 0;
+    int   x = 0;
+    int   y = 0;
+    int   i = 0;
+
+    fptr = soundfile;
+    dptr = strtok(data, "\"");
+    /*
+     * Is data a blank line (ending with LF) or is it a quoted, empty string?
+     */
+    if (dptr == NULL) {
+        fprintf(stderr, "Sound/music command does not contain any data.\n");
+        return -1;
+    }
+    /*
+     * If the first character is not a quote character, a sound command is
+     * expected.
+     */
+    if (data[0] != '\"') {
+        /*
+         * There are 5 numeric values expected and required.  Technically, if
+         * cfsndserv was new, and the client old, 4 might be present, but the
+         * player does not attempt to support old clients.
+         */
+        i = sscanf(dptr, "%d %d %d %d %d", &x, &y, &dir, &vol, &type);
+
+        if ((i != 5)
+        ||  (dir < 0)
+        ||  (dir > 8)
+        ||  (vol < 0)
+        ||  (vol > 100)
+        ||  (type < 1)) {
+            /*
+             * There is not much point in trying to work with data that does
+             * not fit some basic rules known at the time of development.
+             */
+            fprintf(stderr, "Unrecognized sound command data format.\n");
+#ifdef SOUND_DEBUG
+            fprintf(stderr,
+                "(%d valid items read) x=%d y=%d dir=%d vol=%d type=%d\n",
+                    i, x, y, dir, vol, type);
+#endif
+            return -1;
+        }
+    }
+    /*
+     * Below this point, when type == 0, a music command is expected, and when
+     * type != 0, a sound command is required.
+     */
+    if (type) {
+        /*
+         * dptr points to the numerics already read, so advance to the string
+         * following the first quote delimiter.  A sound source name is
+         * expected.
+         */
+        dptr = strtok(NULL, "\"");
+        if (dptr == NULL) {
+            fprintf(stderr, "Sound command is missing sound/source names.\n");
+            return -1;
+        }
+        source = dptr;
+        sourcelen = strlen(dptr);
+       /*
+        * Verify there is whitespace between source and sound names.
+        */
+        dptr = strtok(NULL, "\"");
+        if (dptr == NULL) {
+             fprintf(stderr, "Sound command is missing the sound name.\n");
+             return -1;
+        }
+        spacelen = strlen(dptr);
+        for (i = 0; i < spacelen; i++) {
+            if (dptr[i] != ' ' && dptr[i] != '\t') {
+                fprintf(stderr, "Invalid characters after source name.\n");
+                return -1;
+            }
+        }
+        /*
+         * Advance the data pointer to the following sound name.
+         */
+         dptr = strtok(NULL, "\"");
+         if (dptr == NULL) {
+             fprintf(stderr, "Sound command is missing the sound name.\n");
+             return -1;
+        }
+    }
+    /*
+     * Record the sound or music name here (type determines which it is).
+     */
+    sound = dptr;
+    soundlen = strlen(dptr);
+    /*
+     * If there was a trailing quote after the sound or music name, there will
+     * be a null there now, and sound[soundlen] should point to the character
+     * just before another null at data[len-1] (that terminates the command).
+     */
+    i = sound - data + soundlen + 1 + 1;
+    if (i - 1 == len) {
+        fprintf(stderr, "Sound or music name does not end with a quote.\n");
+        return -1;
+    }
+    if (i > len) {
+        fprintf(stderr,
+            "Invalid data after sound/music name (a quoted string needed)\n");
+        return -1;
+    }
+
+    if (type) {
+        /*
+         * Play sound effect here.
+         */
+#ifdef SOUND_DEBUG
+        fprintf(stderr, "Playing sound "
+            "%d,%d dir=%d vol=%d type=%d source=\"%s\" sound=\"%s\"\n",
+                x, y, dir, vol, type, source, sound);
+#endif
+        play_sound(sound_to_soundnum(sound, type),
+                   type_to_soundtype(type), x, y);
+        return 0;
+    } else {
+        /*
+         * Play music here.
+         */
+#ifdef SOUND_DEBUG
+        fprintf(stderr,
+            "Playing music \"%s\"\n", sound);
+#endif
+        play_music(sound);
+    }
+
     return 0;
 }
 
@@ -436,19 +667,8 @@ int SoundCmd(unsigned char *data, int len) {
  */
 int write_settings(void) {
     FILE *f;
-    char *home;
-    char *path;
 
-    if ((home = getenv("HOME")) == NULL)
-        return -1;
-    path = (char *)malloc(strlen(home) + strlen(CONFIG_FILE) + 1);
-    if (!path)
-        return -1;
-
-    strcpy(path, home);
-    strcat(path, CONFIG_FILE);
-    f = fopen(path, "w");
-
+    f = fopen(user_config_file, "w");
     if (!f)
         return -1;
 
@@ -472,21 +692,13 @@ int write_settings(void) {
  * @return
  */
 int read_settings(void) {
-    FILE *f;
-    char *home;
-    char *path;
     char linebuf[1024];
+    FILE *f;
 
-    if ((home = getenv("HOME")) == NULL )
+    if (user_config_file == NULL)
         return 0;
 
-    path = (char *)malloc(strlen(home) + strlen(CONFIG_FILE) + 1);
-    if (!path)
-        return 0;
-
-    strcpy(path, home);
-    strcat(path, CONFIG_FILE);
-    f = fopen(path, "r");
+    f = fopen(user_config_file, "r");
     if (!f)
         return -1;
 
