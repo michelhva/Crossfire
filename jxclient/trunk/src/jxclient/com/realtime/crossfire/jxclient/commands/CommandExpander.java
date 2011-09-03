d5ab2870b8218c8e025dcca8f31b1ead1ff4f5d9
akirschbaum <akirschbaum@282e977c-c81d-0410-88c4-b93c2d0d6712>
2011-09-03 07:18:08 +0000

    Extract command parser into separate class.

---
+++ jxclient/trunk/src/jxclient/com/realtime/crossfire/jxclient/commands/CommandExpander.java
@@ -0,0 +1,90 @@
+/*
+ * This file is part of JXClient, the Fullscreen Java Crossfire Client.
+ *
+ * JXClient is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * JXClient is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with JXClient; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * Copyright (C) 2005-2008 Yann Chachkoff.
+ * Copyright (C) 2006-2011 Andreas Kirschbaum.
+ */
+
+package com.realtime.crossfire.jxclient.commands;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Expands a command (or list of commands) into a sequence of {@link CommandExec
+ * Commands} to execute.
+ * @author Andreas Kirschbaum
+ */
+public class CommandExpander {
+
+    /**
+     * The {@link Pattern} for splitting command sequences.
+     */
+    @NotNull
+    private static final Pattern PATTERN_SEPARATOR = Pattern.compile(" *; *");
+
+    /**
+     * The {@link Pattern} for splitting commands from command arguments.
+     */
+    @NotNull
+    private static final Pattern PATTERN_SPACES = Pattern.compile(" +");
+
+    /**
+     * Private constructor to prevent instantiation.
+     */
+    private CommandExpander() {
+    }
+
+    /**
+     * Expands a command list into a sequence of {@link CommandExec Commands} to
+     * execute.
+     * @param commandList the commands to split
+     * @param commands the commands to use
+     * @return the list of commands
+     */
+    public static List<CommandExec> expand(@NotNull final CharSequence commandList, @NotNull final Commands commands) {
+        final List<CommandExec> list = new ArrayList<CommandExec>();
+        CharSequence remainingCommandList = commandList;
+        while (true) {
+            final String[] tmp = PATTERN_SEPARATOR.split(remainingCommandList, 2);
+            final String commandSpec = tmp[0];
+            if (!commandSpec.isEmpty()) {
+                final String[] tmp2 = PATTERN_SPACES.split(commandSpec, 2);
+                final String commandName = tmp2[0];
+                final String commandArgs = tmp2.length == 2 ? tmp2[1] : "";
+                final Command command = commands.findCommand(commandName);
+                if (command == null) {
+                    list.add(new CommandExec(null, commandSpec));
+                } else if (command.allArguments()) {
+                    final String[] tmp3 = PATTERN_SPACES.split(remainingCommandList, 2);
+                    list.add(new CommandExec(command, tmp3.length == 2 ? tmp3[1] : ""));
+                    break;
+                } else {
+                    list.add(new CommandExec(command, commandArgs));
+                }
+            }
+            if (tmp.length < 2) {
+                break;
+            }
+            remainingCommandList = tmp[1];
+        }
+        return list;
+    }
+
+}
---
+++ jxclient/trunk/src/test/com/realtime/crossfire/jxclient/commands/CommandExpanderTest.java
@@ -0,0 +1,191 @@
+/*
+ * This file is part of JXClient, the Fullscreen Java Crossfire Client.
+ *
+ * JXClient is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * JXClient is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with JXClient; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * Copyright (C) 2005-2008 Yann Chachkoff.
+ * Copyright (C) 2006-2011 Andreas Kirschbaum.
+ */
+
+package com.realtime.crossfire.jxclient.commands;
+
+import com.realtime.crossfire.jxclient.guistate.GuiStateManager;
+import com.realtime.crossfire.jxclient.queue.CommandQueue;
+import com.realtime.crossfire.jxclient.server.crossfire.CrossfireServerConnection;
+import com.realtime.crossfire.jxclient.server.crossfire.TestCrossfireServerConnection;
+import java.util.Arrays;
+import java.util.Collection;
+import org.jetbrains.annotations.NotNull;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Regression test for {@link CommandExpander}.
+ * @author Andreas Kirschbaum
+ */
+public class CommandExpanderTest {
+
+    /**
+     * Checks that {@link CommandExpander#expand(CharSequence, Commands)} works
+     * as expected.
+     */
+    @Test
+    public void testExpandSingle() {
+        final CrossfireServerConnection crossfireServerConnection = new TestCrossfireServerConnection();
+        final GuiStateManager guiStateManager = new GuiStateManager(crossfireServerConnection);
+        final CommandQueue commandQueue = new CommandQueue(crossfireServerConnection, guiStateManager);
+        final Commands commands = new Commands(commandQueue);
+        check("xyz", commands, new CommandExec(null, "xyz"));
+    }
+
+    /**
+     * Checks that {@link CommandExpander#expand(CharSequence, Commands)} works
+     * as expected.
+     */
+    @Test
+    public void testExpandMultiple() {
+        final CrossfireServerConnection crossfireServerConnection = new TestCrossfireServerConnection();
+        final GuiStateManager guiStateManager = new GuiStateManager(crossfireServerConnection);
+        final CommandQueue commandQueue = new CommandQueue(crossfireServerConnection, guiStateManager);
+        final Commands commands = new Commands(commandQueue);
+        check("xyz;abc;abc", commands, new CommandExec(null, "xyz"), new CommandExec(null, "abc"), new CommandExec(null, "abc"));
+    }
+
+    /**
+     * Checks that {@link CommandExpander#expand(CharSequence, Commands)} allows
+     * whitespace before or after the command separator.
+     */
+    @Test
+    public void testExpandIgnoreWhitespace() {
+        final CrossfireServerConnection crossfireServerConnection = new TestCrossfireServerConnection();
+        final GuiStateManager guiStateManager = new GuiStateManager(crossfireServerConnection);
+        final CommandQueue commandQueue = new CommandQueue(crossfireServerConnection, guiStateManager);
+        final Commands commands = new Commands(commandQueue);
+        check("xyz;abc", commands, new CommandExec(null, "xyz"), new CommandExec(null, "abc"));
+        check("xyz ;abc", commands, new CommandExec(null, "xyz"), new CommandExec(null, "abc"));
+        check("xyz; abc", commands, new CommandExec(null, "xyz"), new CommandExec(null, "abc"));
+        check("xyz   ;   abc", commands, new CommandExec(null, "xyz"), new CommandExec(null, "abc"));
+    }
+
+    /**
+     * Checks that {@link CommandExpander#expand(CharSequence, Commands)}
+     * ignores empty commands.
+     */
+    @Test
+    public void testExpandIgnoreEmpty() {
+        final CrossfireServerConnection crossfireServerConnection = new TestCrossfireServerConnection();
+        final GuiStateManager guiStateManager = new GuiStateManager(crossfireServerConnection);
+        final CommandQueue commandQueue = new CommandQueue(crossfireServerConnection, guiStateManager);
+        final Commands commands = new Commands(commandQueue);
+        check("", commands);
+        check(";;;", commands);
+        check("   ;   xyz   ;   abc   ;   ", commands, new CommandExec(null, "xyz"), new CommandExec(null, "abc"));
+    }
+
+    /**
+     * Checks that {@link CommandExpander#expand(CharSequence, Commands)}
+     * detects predefined commands.
+     */
+    @Test
+    public void testExpandPredefined() {
+        final CrossfireServerConnection crossfireServerConnection = new TestCrossfireServerConnection();
+        final GuiStateManager guiStateManager = new GuiStateManager(crossfireServerConnection);
+        final CommandQueue commandQueue = new CommandQueue(crossfireServerConnection, guiStateManager);
+        final Commands commands = new Commands(commandQueue);
+        final TestCommand cmd = new TestCommand("cmd", false, crossfireServerConnection);
+        commands.addCommand(cmd);
+        check("cmd", commands, new CommandExec(cmd, ""));
+        check("cmd abc", commands, new CommandExec(cmd, "abc"));
+        check("cm", commands, new CommandExec(null, "cm"));
+        check("cmd2", commands, new CommandExec(null, "cmd2"));
+        check("cmd;cmd;cm;cmd;cmd2", commands, new CommandExec(cmd, ""), new CommandExec(cmd, ""), new CommandExec(null, "cm"), new CommandExec(cmd, ""), new CommandExec(null, "cmd2"));
+    }
+
+    /**
+     * Checks that {@link CommandExpander#expand(CharSequence, Commands)}
+     * correctly parses command arguments.
+     */
+    @Test
+    public void testExpandArguments() {
+        final CrossfireServerConnection crossfireServerConnection = new TestCrossfireServerConnection();
+        final GuiStateManager guiStateManager = new GuiStateManager(crossfireServerConnection);
+        final CommandQueue commandQueue = new CommandQueue(crossfireServerConnection, guiStateManager);
+        final Commands commands = new Commands(commandQueue);
+        final TestCommand cmd = new TestCommand("cmd", false, crossfireServerConnection);
+        commands.addCommand(cmd);
+        check("cmd", commands, new CommandExec(cmd, ""));
+        check("cmd abc", commands, new CommandExec(cmd, "abc"));
+        check("cmd    abc", commands, new CommandExec(cmd, "abc"));
+        check("cm", commands, new CommandExec(null, "cm"));
+        check("cm abc", commands, new CommandExec(null, "cm abc"));
+        check("cm   abc", commands, new CommandExec(null, "cm   abc"));
+    }
+
+    /**
+     * Checks that {@link CommandExpander#expand(CharSequence, Commands)}
+     * matches command names case-insensitive. This is what the server does,
+     * too.
+     */
+    @Test
+    public void testExpandCaseInsensitiveCommandNames() {
+        final CrossfireServerConnection crossfireServerConnection = new TestCrossfireServerConnection();
+        final GuiStateManager guiStateManager = new GuiStateManager(crossfireServerConnection);
+        final CommandQueue commandQueue = new CommandQueue(crossfireServerConnection, guiStateManager);
+        final Commands commands = new Commands(commandQueue);
+        final TestCommand cmd = new TestCommand("cmd", false, crossfireServerConnection);
+        commands.addCommand(cmd);
+        check("cmd", commands, new CommandExec(cmd, ""));
+        check("Cmd", commands, new CommandExec(cmd, ""));
+        check("cmD", commands, new CommandExec(cmd, ""));
+        check("CMD", commands, new CommandExec(cmd, ""));
+    }
+
+    /**
+     * Checks that {@link CommandExpander#expand(CharSequence, Commands)}
+     * correctly parses commands that consume all remaining arguments.
+     */
+    @Test
+    public void testExpandAllArguments() {
+        final CrossfireServerConnection crossfireServerConnection = new TestCrossfireServerConnection();
+        final GuiStateManager guiStateManager = new GuiStateManager(crossfireServerConnection);
+        final CommandQueue commandQueue = new CommandQueue(crossfireServerConnection, guiStateManager);
+        final Commands commands = new Commands(commandQueue);
+        final TestCommand cmd = new TestCommand("cmd", false, crossfireServerConnection);
+        final TestCommand bind = new TestCommand("bind", true, crossfireServerConnection);
+        commands.addCommand(cmd);
+        commands.addCommand(bind);
+        check("cmd bind", commands, new CommandExec(cmd, "bind"));
+        check("bind cmd", commands, new CommandExec(bind, "cmd"));
+        check("cmd bind;bind cmd", commands, new CommandExec(cmd, "bind"), new CommandExec(bind, "cmd"));
+        check("bind cmd;cmd bind", commands, new CommandExec(bind, "cmd;cmd bind"));
+        check("cmd bind;bind cmd;cmd bind", commands, new CommandExec(cmd, "bind"), new CommandExec(bind, "cmd;cmd bind"));
+        check("bind cmd;cmd bind;bind cmd", commands, new CommandExec(bind, "cmd;cmd bind;bind cmd"));
+        check("cmd bind ; bind cmd ; cmd bind", commands, new CommandExec(cmd, "bind"), new CommandExec(bind, "cmd ; cmd bind"));
+        check("bind cmd ; cmd bind ; bind cmd", commands, new CommandExec(bind, "cmd ; cmd bind ; bind cmd"));
+    }
+
+    /**
+     * Checks that {@link CommandExpander#expand(CharSequence, Commands)} works
+     * as expected.
+     * @param command the command to split
+     * @param commands the commands instance to use
+     * @param commandList the expected result
+     */
+    private static void check(@NotNull final CharSequence command, @NotNull final Commands commands, @NotNull final CommandExec... commandList) {
+        final Collection<CommandExec> expandedCommands = CommandExpander.expand(command, commands);
+        Assert.assertEquals(Arrays.asList(commandList), expandedCommands);
+    }
+
+}
