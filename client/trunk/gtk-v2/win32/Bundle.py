b605ad5c6a2da9a3d47c5b59ebcf348726ede9c9
alestan <alestan@282e977c-c81d-0410-88c4-b93c2d0d6712>
2011-09-20 07:18:45 +0000

    Added scripts for building gtk2 client on windows

---
+++ client/trunk/gtk-v2/win32/Bundle.py
@@ -0,0 +1,47 @@
+import os, sys, subprocess
+import urllib2 as u
+pwd=os.getcwd()
+def system(cmd):
+	return subprocess.Popen(cmd.split(),env=os.environ,stdin=sys.stdin,stdout=sys.stdout,stderr=sys.stderr).wait()
+def AskInstall(Name):
+	return Auto or 'Y' in raw_input('Install %s?' %Name).upper()
+Auto='auto' in sys.argv
+
+def FetchFile(URL, Filename):
+	f=u.urlopen(URL)
+	o=open(Filename,'wb')
+	txt=f.read(1024*512)
+	ctr=0
+	while txt:
+		if (ctr+1)/2*2==ctr+1:
+			print ctr*1024*512+len(txt), 'bytes downloaded'
+		o.write(txt)	
+		txt=f.read()
+		ctr+=1
+	f.close()
+	o.close()
+
+
+#FetchFile('https://crossfire.svn.sourceforge.net/svnroot/crossfire/client/trunk/gtk-v2/win32/gtkclient.nsi', 'gtkclient.nsi')
+
+
+if AskInstall('Nsis'):
+	FetchFile('http://downloads.sourceforge.net/project/nsis/NSIS%202/2.46/nsis-2.46-setup.exe?r=http%3A%2F%2Fnsis.sourceforge.net%2FDownload&ts=1316497068&use_mirror=superb-sea2', 'nsis.exe')
+	system('nsis.exe')
+
+NSIS=raw_input('Path to nsis?') or pwd+'\NSIS'
+NSIS+='\makensis.exe '
+if 'CFSOURCE' not in dir():
+	CFSOURCE=raw_input("Path to client.svn? ")
+f=open('gtkclient.nsi.in')
+txt=f.read()
+f.close()
+txt=txt %(CFSOURCE,CFSOURCE,CFSOURCE,CFSOURCE,CFSOURCE,CFSOURCE,CFSOURCE,CFSOURCE,CFSOURCE,CFSOURCE,CFSOURCE,CFSOURCE,CFSOURCE,CFSOURCE,CFSOURCE,CFSOURCE,CFSOURCE,CFSOURCE)
+f=open('gtkclient.nsi','w')
+f.write(txt)
+f.close()
+
+print NSIS
+
+system(NSIS + 'gtkclient.nsi')
+
