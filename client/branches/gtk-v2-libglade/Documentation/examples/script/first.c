3da2676d3af4a6fe19a5f871c3e56166d28b4b95
partmedia <partmedia@282e977c-c81d-0410-88c4-b93c2d0d6712>
2013-09-12 21:34:15 +0000

    Remove feature branch merged in long ago.

--- client/branches/gtk-v2-libglade/Documentation/examples/script/first.c
+++
@@ -1,3 +0,0 @@
-int main(){
-	printf("issue 1 1 say hello world\n");
-}
--- client/branches/gtk-v2-libglade/common/version.h
+++
@@ -1,7 +0,0 @@
-#include <svnversion.h>
-
-#ifdef SVN_REV
-#define FULL_VERSION VERSION"-r"SVN_REV
-#else
-#define FULL_VERSION VERSION
-#endif
