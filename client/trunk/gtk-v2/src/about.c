f5f0a04addc192841b98a728438410d0ce6b1da1
partmedia <partmedia@282e977c-c81d-0410-88c4-b93c2d0d6712>
2014-11-24 23:10:09 +0000

    Redesign about dialog using built-in about widget

--- client/trunk/gtk-v2/src/about.c
+++ client/trunk/gtk-v2/src/about.c
@@ -16,21 +16,9 @@
  * Supports the client's about box dialog.
  */
 
-#ifdef HAVE_CONFIG_H
-#include <config.h>
-#endif
-
-#include <ctype.h>
 #include <gtk/gtk.h>
 
-#include "client.h"
-
 #include "main.h"
-#include "image.h"
-#include "gtk2proto.h"
-#include "../../pixmaps/crossfiretitle.xpm"
-
-static GtkWidget *about_window = NULL;
 
 /**
  * Instantiates and displays the client's about box dialog.
@@ -39,31 +27,8 @@ static GtkWidget *about_window = NULL;
  * @param user_data
  */
 void menu_about(GtkMenuItem *menuitem, gpointer user_data) {
-    if (!about_window) {
-        GtkImage *about_image;
-
-        about_window = GTK_WIDGET(gtk_builder_get_object(dialog_xml, "about_window"));
-        about_image = GTK_IMAGE(gtk_builder_get_object(dialog_xml, "about_image"));
-
-        g_signal_connect((gpointer) about_window, "delete_event",
-                         G_CALLBACK(gtk_widget_hide_on_delete), NULL);
-
-        /* Load Crossfire image from inline XPM. */
-        gtk_image_set_from_pixbuf(about_image, gdk_pixbuf_new_from_xpm_data(
-                    (const char **)crossfiretitle_xpm));
-
-        gtk_widget_show(about_window);
-    } else {
-        gtk_widget_show(about_window);
-    }
-}
-
-/**
- * Closes and hides the client's about box dialog.
- *
- * @param button    The about dialog's close button.
- * @param user_data
- */
-void on_about_close_clicked(GtkButton *button, gpointer user_data) {
+    GtkWidget *about_window;
+    about_window = GTK_WIDGET(gtk_builder_get_object(dialog_xml, "about_window"));
+    gtk_dialog_run(GTK_DIALOG(about_window));
     gtk_widget_hide(about_window);
 }
