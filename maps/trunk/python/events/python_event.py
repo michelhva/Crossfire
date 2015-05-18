746e9b7941056643b71bb982ad00194ab904c653
silvernexus <silvernexus@282e977c-c81d-0410-88c4-b93c2d0d6712>
2015-05-18 15:53:34 +0000

    Tweak documentation to match the actual file name.

--- maps/trunk/python/events/python_event.py
+++ maps/trunk/python/events/python_event.py
@@ -1,4 +1,4 @@
-# global_event.py - this script handles all global events, except the plugin init (check plugin_init.py for that).
+# python_event.py - this script handles all global events, except the plugin init (check plugin_init.py for that).
 #
 # This script merely looks and launches scripts in a specific subdirectory of /python/events/
 # Only .py files are considered, you can simply rename a file to disable.
