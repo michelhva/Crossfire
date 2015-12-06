85328735c9847542e7f2547379b55508abb9dde0
akirschbaum <akirschbaum@282e977c-c81d-0410-88c4-b93c2d0d6712>
2015-12-06 18:06:57 +0000

    Move dependency to Swing from model to gui package.

--- jxclient/trunk/src/gui/src/main/java/com/realtime/crossfire/jxclient/gui/item/GUIItemInventory.java
+++ jxclient/trunk/src/gui/src/main/java/com/realtime/crossfire/jxclient/gui/item/GUIItemInventory.java
@@ -33,6 +33,7 @@ import com.realtime.crossfire.jxclient.items.LocationListener;
 import com.realtime.crossfire.jxclient.queue.CommandQueue;
 import com.realtime.crossfire.jxclient.server.crossfire.CrossfireServerConnection;
 import java.awt.Image;
+import javax.swing.SwingUtilities;
 import org.jetbrains.annotations.NotNull;
 
 /**
@@ -106,6 +107,19 @@ public class GUIItemInventory extends GUIItemItem {
 
         @Override
         public void locationChanged() {
+            SwingUtilities.invokeLater(locationListenerEdt);
+        }
+
+    };
+
+    /**
+     * Called from {@link #locationListener} but runs on the EDT.
+     */
+    @NotNull
+    private final Runnable locationListenerEdt = new Runnable() {
+
+        @Override
+        public void run() {
             setChanged();
             updateTooltipText();
         }
