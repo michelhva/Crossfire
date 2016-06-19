0e4a210ba7b7be9923fc9efc01ba34e92a188f5d
akirschbaum <akirschbaum@282e977c-c81d-0410-88c4-b93c2d0d6712>
2016-06-19 15:12:01 +0000

    Do not mix spaces and TABs for indentation.

--- maps/trunk/python/misc/swallow_whole.py
+++ maps/trunk/python/misc/swallow_whole.py
@@ -11,30 +11,30 @@ r  = random.random()
 
 if  (r <= 0.26):
     if (me.__class__ is Crossfire.Player):
-	isplayer = 1
+        isplayer = 1
     elif (me.Alive == True):
-	isplayer = 0
+        isplayer = 0
     else:
-	isplayer = 2
+        isplayer = 2
 
     if isplayer == 1:
-	me.Map.Print("\nYou are swallowed whole by the %s!" % (worm.Name))
-	map = Crossfire.ReadyMap(eatmap)
-	if map:
-	    me.Teleport(map, eat_x, eat_y)
-	else:
-	    Crossfire.Log(Crossfire.LogError, "There is no eat map")
+        me.Map.Print("\nYou are swallowed whole by the %s!" % (worm.Name))
+        map = Crossfire.ReadyMap(eatmap)
+        if map:
+            me.Teleport(map, eat_x, eat_y)
+        else:
+            Crossfire.Log(Crossfire.LogError, "There is no eat map")
 
     elif isplayer == 0:
-	me.Map.Print("\nThe %s is swallowed whole by the %s!" % (me.Name, worm.Name))
+        me.Map.Print("\nThe %s is swallowed whole by the %s!" % (me.Name, worm.Name))
 
-	while (me.Inventory is not None):
-	    me.Inventory.InsertInto(worm)
+        while (me.Inventory is not None):
+            me.Inventory.InsertInto(worm)
 
-	mexists = Crossfire.WhoIsOther()
-	if mexists:
-	    me.Remove()
-	else:
-	    worm.Map.Print('doesnt exist')
+        mexists = Crossfire.WhoIsOther()
+        if mexists:
+            me.Remove()
+        else:
+            worm.Map.Print('doesnt exist')
 else:
     me.Map.Print("\nThe %s misses the %s" % (worm.Name, me.Name))
