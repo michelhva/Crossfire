fd2af5ec6d9ae0207c0f41620fb2e7e70469bd85
partmedia <partmedia@282e977c-c81d-0410-88c4-b93c2d0d6712>
2017-07-23 19:03:13 +0000

    Update shared header from server

--- client/trunk/common/shared/newclient.h
+++ client/trunk/common/shared/newclient.h
@@ -1,31 +1,3 @@
-/*
- * static char *rcsid_newclient_h =
- *   "$Id$";
- */
-
-/*
-    CrossFire, A Multiplayer game for X-windows
-
-    Copyright (C) 2002,2006,2010 Mark Wedel & Crossfire Development Team
-    Copyright (C) 1992 Frank Tore Johansen
-
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-
-    The authors can be reached via e-mail at crossfire-devel@real-time.com
-*/
-
 /**
  * @file
  * Defines various flags that both the new client and new server use.  These
@@ -324,12 +296,15 @@ enum {a_none, a_readied, a_wielded, a_worn, a_active, a_applied};
 /*@}*/
 
 /**
- * @defgroup SOUND_xxx SOUND_xxx constants.
- *
+ * @defgroup Soundtypes Sound types
  */
 /*@{*/
-#define SOUND_NORMAL    0
-#define SOUND_SPELL     1
+#define SOUND_TYPE_LIVING       1
+#define SOUND_TYPE_SPELL        2
+#define SOUND_TYPE_ITEM         3
+#define SOUND_TYPE_GROUND       4
+#define SOUND_TYPE_HIT          5
+#define SOUND_TYPE_HIT_BY       6
 /*@}*/
 
 /**
@@ -672,7 +647,7 @@ enum {a_none, a_readied, a_wielded, a_worn, a_active, a_applied};
 /**
  * Contains the base information we use to make up a packet we want to send.
  */
-typedef struct {
+typedef struct SockList {
 #ifdef CLIENT_TYPES_H                       /* Used by the client */
     int len;
     unsigned char *buf;
@@ -685,7 +660,7 @@ typedef struct {
 /**
  * Statistics on server.
  */
-typedef struct {
+typedef struct CS_Stats {
     int     ibytes;     /**< ibytes, obytes are bytes in, out. */
     int     obytes;
     short   max_conn;   /**< Maximum connections received. */
