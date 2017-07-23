e2802dba76d74778f0c31d31ecef431235af4784
partmedia <partmedia@282e977c-c81d-0410-88c4-b93c2d0d6712>
2017-07-23 19:03:06 +0000

    Store sound mapping in a hash table

--- client/trunk/sound-src/cfsndserv.c
+++ client/trunk/sound-src/cfsndserv.c
@@ -20,16 +20,10 @@
 
 #include <SDL.h>
 #include <SDL_mixer.h>
+#include <glib-object.h>
 
 #include "common.h"
-
-#define MAX_SOUNDS 1024
-
-typedef struct Sound_Info {
-    char *filename;
-    char *symbolic;
-    unsigned char volume;
-} Sound_Info;
+#include "snd.h"
 
 typedef struct sound_settings {
     int buflen;     //< how big the buffers should be
@@ -39,41 +33,8 @@ typedef struct sound_settings {
 static Mix_Music *music = NULL;
 static sound_settings settings = { 512, 4 };
 
-static Sound_Info sounds[MAX_SOUNDS];
-
 static GHashTable* chunk_cache;
-
-/**
- * Convert a sound name to a sound number to help with the transition of the
- * sound server from sound support to sound2 capability.  This is not an end
- * solution, but one that gets the sound server working a little bit until a
- * better one can be implemented.
- */
-static int sound_to_soundnum(const char *name, guint8 type) {
-    for (int i = 0; i < MAX_SOUNDS; i++) {
-        if (sounds[i].symbolic != NULL) {
-            if (strcmp(sounds[i].symbolic, name) == 0) {
-                return i;
-            }
-        }
-    }
-    printf("Could not find matching sound for '%s'.\n", name);
-    return -1;
-}
-
-/**
- * Convert a legacy sound type to the sound2 equivalent.
- *
- * This is intended to help ease the transition from old sound to sound2
- * capability.
- */
-static int type_to_soundtype(guint8 type) {
-    if (type == 2) {
-        return 1;
-    } else {
-        return 0;
-    }
-}
+static GHashTable* sounds;
 
 /**
  * Initialize the sound subsystem.
@@ -109,57 +70,6 @@ static int init_audio() {
 }
 
 /**
- * Load sound definitions from a file.
- */
-static void init_sounds() {
-    /* Initialize by setting all sounds to NULL. */
-    for (int i = 0; i < MAX_SOUNDS; i++) {
-        sounds[i].filename = NULL;
-    }
-
-    /* Try to open the sound definitions file. */
-    FILE *fp = fopen(g_getenv("CF_SOUND_CONF"), "r");
-    if (fp == NULL) {
-        fprintf(stderr, "Could not find sound definitions; aborting!\n");
-        exit(EXIT_FAILURE);
-    }
-    printf("Loaded sounds from '%s'\n", g_getenv("CF_SOUND_DIR"));
-
-    /* Use 'i' as index tracker, so set it to zero. */
-    int i = 0;
-
-    /* Parse the sound definitions file, line by line. */
-    char buf[512];
-    while (fgets(buf, sizeof(buf), fp) != NULL) {
-        char *line;
-        line = &buf[0];
-
-        /* Ignore all lines that start with a comment or newline. */
-        if (buf[0] == '#' || buf[0] == '\n') {
-            continue;
-        }
-
-        /* Trim the trailing newline if it exists (see CERT FIO36-C). */
-        char *newline;
-        newline = strchr(buf, '\n');
-
-        if (newline != NULL) {
-            *newline = '\0';
-        }
-
-        /* FIXME: No error checking; potential segfaults here. */
-        sounds[i].symbolic = g_strdup(strsep(&line, ":"));
-        sounds[i].volume = atoi(strsep(&line, ":"));
-        sounds[i].filename = g_strdup(strsep(&line, ":"));
-
-        /* Move on to the next sound. */
-        i++;
-    }
-
-    fclose(fp);
-}
-
-/**
  * Initialize sound server.
  *
  * Initialize resource paths, load sound definitions, and ready the sound
@@ -174,19 +84,13 @@ int cf_snd_init() {
         return -1;
     }
 
-    /* Set $CF_SOUND_CONF to something reasonable, if not already set. */
-    char path[MAXSOCKBUF];
-    snprintf(path, sizeof(path), "%s/sounds.conf", g_getenv("CF_SOUND_DIR"));
-
-    if (!g_setenv("CF_SOUND_CONF", path, FALSE)) {
-        perror("Couldn't set $CF_SOUND_CONF");
-        return -1;
-    }
-
     /* Initialize sound definitions. */
     chunk_cache = g_hash_table_new_full(g_str_hash, g_str_equal, NULL,
                                         (void *)Mix_FreeChunk);
-    init_sounds();
+    sounds = load_snd_config();
+    if (!sounds) {
+        return -1;
+    }
 
     /* Initialize audio library. */
     if (init_audio()) {
@@ -217,51 +121,30 @@ static Mix_Chunk* load_chunk(char const name[static 1]) {
 /**
  * Play a sound effect using the SDL_mixer sound system.
  *
- * @param soundnum  The sound to play.
- * @param soundtype 0 for normal sounds, 1 for spell_sounds.
+ * @param sound     The sound to play.
+ * @param type      0 for normal sounds, 1 for spell_sounds.
  * @param x         Offset (assumed from player) to play sound used to
  *                  determine value and left vs. right speaker balance.
  * @param y         Offset (assumed from player) to play sound used to
  *                  determine value and left vs. right speaker balance.
  */
-static void play_sound(int soundnum, int soundtype, int x, int y) {
-    /* Ignore commands to play invalid sound types. */
-    if (soundnum >= MAX_SOUNDS || soundnum < 0) {
-        fprintf(stderr, "play_sound: Invalid sound number: %d\n", soundnum);
-        return;
-    }
-
+void cf_play_sound(gint8 x, gint8 y, guint8 dir, guint8 vol, guint8 type,
+                   char const sound[static 1], char const source[static 1]) {
     /* Do not play sound if the channel limit has been reached. */
     if (Mix_Playing(-1) >= settings.max_chunk) {
         fprintf(stderr, "play_sound: too many sounds are already playing\n");
         return;
     }
 
-    /* Refuse to play a sound with an invalid type. */
-    Sound_Info *si;
-    if (soundtype == SOUND_NORMAL || soundtype == SOUND_SPELL) {
-        si = &sounds[soundnum];
-    } else {
-        fprintf(stderr,"play_sound: Unknown soundtype: %d\n", soundtype);
+    SoundInfo* si = g_hash_table_lookup(sounds, sound);
+    if (si == NULL) {
+        fprintf(stderr,"play_sound: sound not defined: %s\n", sound);
         return;
     }
-
-    /* Refuse to play a sound without a name. */
-    if (!si->filename) {
-        fprintf(stderr, "play_sound: Sound %d (type %d) missing\n",
-                soundnum, soundtype);
-        return;
-    }
-
-    Mix_Chunk* chunk = load_chunk(si->filename);
+    Mix_Chunk* chunk = load_chunk(si->file);
     if (chunk != NULL) {
         Mix_PlayChannel(-1, chunk, 0);
     }
-}
-
-void cf_play_sound(gint8 x, gint8 y, guint8 dir, guint8 vol, guint8 type,
-                   char const sound[static 1], char const source[static 1]) {
-    play_sound(sound_to_soundnum(sound, type), type_to_soundtype(type), x, y);
 }
 
 static bool music_is_different(char const music[static 1]) {
@@ -309,6 +192,7 @@ void cf_snd_exit() {
     /* Halt all channels that are playing and free remaining samples. */
     Mix_HaltChannel(-1);
     g_hash_table_destroy(chunk_cache);
+    g_hash_table_destroy(sounds);
 
     /* Call Mix_Quit() for each time Mix_Init() was called. */
     while(Mix_Init(0)) {
