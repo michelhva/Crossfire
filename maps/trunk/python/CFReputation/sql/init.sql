5e8be4ef4a2191481716b28423e3c33f746fe2cb
partmedia <partmedia@282e977c-c81d-0410-88c4-b93c2d0d6712>
2018-12-30 00:55:26 +0000

    Separate schema files from init files

--- maps/trunk/python/CFReputation/sql/init.sql
+++ maps/trunk/python/CFReputation/sql/init.sql
@@ -1,36 +1,11 @@
-CREATE TABLE IF NOT EXISTS schema(version INT);
-
-CREATE TABLE regions(
-    faction      TEXT,
-    region       TEXT,    -- region name (or 'ALL') this faction controls
-    influence    NUMERIC,
-    CONSTRAINT   influence_range CHECK(influence BETWEEN 0 AND 1)
-);
-
-CREATE TABLE relations(
-    faction      TEXT,
-    race         TEXT,
-    attitude     NUMERIC,
-    PRIMARY KEY  (faction, race),
-    CONSTRAINT   attitude_range CHECK(attitude BETWEEN -1 AND 1)
-);
-
-CREATE TABLE reputations(
-    name         TEXT,          -- player name
-    faction      TEXT,
-    reputation   NUMERIC,
-    PRIMARY KEY  (name, faction),
-    CONSTRAINT   reputation_range CHECK(reputation BETWEEN -1 AND 1)
-);
-
-INSERT INTO regions VALUES
+INSERT OR REPLACE INTO regions VALUES
 ('Dragons', 'ALL', 0.4),
 ('Scorn', 'scorn', 0.5),
 ('Scorn', 'scornarena', 0.5),
 ('Scorn', 'scorncounty', 0.5),
 ('Scorn', 'scornoldcity', 0.5);
 
-INSERT INTO relations VALUES
+INSERT OR REPLACE INTO relations VALUES
 ('Dragons', 'dragon', 1),
 ('Dragons', 'faerie', -1),
 ('Dragons', 'human', -1),
@@ -43,5 +18,3 @@ INSERT INTO relations VALUES
 ('Scorn', 'troll', -1),
 ('Scorn', 'undead', -1),
 ('Scorn', 'unnatural', -1);
-
-INSERT INTO schema VALUES(1);
